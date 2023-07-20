d1dd5f3c486b04ca3731ab5c346c34ac5e8d8d99
Erez Rokah <erezrokah@users.noreply.github.com>
2023-07-20 11:10:48 +0200

    chore(snowflake-policies): Implement `create-view` command (#1)

---
+++ aws/foundational_security/snowflake/views.py
@@ -0,0 +1,11 @@
+CREATE_AWS_POLICY_RESULTS = """
+create table if not exists aws_policy_results (
+    execution_time timestamp with time zone,
+    framework varchar(255),
+    check_id varchar(255),
+    title text,
+    account_id varchar(1024),
+    resource_id varchar(1024),
+    status varchar(16)
+)
+"""
