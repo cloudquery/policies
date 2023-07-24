eb533ce6a8701f6b263583f4a4a632efa7ad6449
Jonathan Sarig <73023114+john-s58@users.noreply.github.com>
2023-07-24 21:08:34 +0300

    added new query to acm.py relevant to acm.2 and updated sections.py to include this query (#11)
    * added new query to acm.py relevant to acm.2 and updated sections.py to include this query
    
    * changed the query to check specifically for RSA 1024
    
    * changed condition to show other RSA certifications as pass and another case of RSA_1024

--- aws/foundational_security/snowflake/queries/acm.py
+++ aws/foundational_security/snowflake/queries/acm.py
@@ -17,3 +17,21 @@ select
   end as status
 FROM aws_acm_certificates
 """
+
+RSA_CERTIFICATE_KEY_LENGTH_SHOULD_BE_MORE_THAN_2048_BITS = """
+ insert into aws_policy_results
+select
+  :1 as execution_time,
+  :2 as framework,
+  :3 as check_id,
+  'rsa certificate key length is less than 2048 bits' as title,
+  account_id,
+  arn AS resource_id,
+  CASE
+  WHEN key_algorithm IN ('RSA-1024', 'RSA 1024', 'RSA_1024')
+  THEN 'fail' 
+  ELSE 'pass'
+  END AS status
+FROM aws_acm_certificates
+WHERE left(key_algorithm, 3) = 'RSA'
+"""
