82d16e387c64bfc0ca623e1b3f41c274aef7c252
Kemal Hadimli <disq@users.noreply.github.com>
2023-07-25 14:31:26 +0100

    fix: Insert column values not strings

--- aws/foundational_security/snowflake/queries/account.py
+++ aws/foundational_security/snowflake/queries/account.py
@@ -8,8 +8,8 @@ SELECT
   :2 as framework,
   :3 as check_id,
   'Security contact information should be provided for an AWS account' AS "title",
-  'aws_iam_accounts.account_id' as "account_id",
-  'aws_iam_accounts.account_id' as "resource_id",
+  aws_iam_accounts.account_id as "account_id",
+  aws_iam_accounts.account_id as "resource_id",
   CASE WHEN
     'alternate_contact_type' IS NULL
     THEN 'fail'
