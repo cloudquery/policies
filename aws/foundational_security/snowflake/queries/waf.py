d99d1b551e6d526c06b93971bda15befced33b39
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-07-30 11:39:06 +0300

    waf_global_webacl_not_empty

--- aws/foundational_security/snowflake/queries/waf.py
+++ aws/foundational_security/snowflake/queries/waf.py
@@ -25,9 +25,26 @@ SELECT
 	account_id,
 	arn,
 	CASE
-	WHEN jsonb_array_length(rules) = 0 THEN 'fail'
+	WHEN ARRAY_SIZE(rules) = 0 THEN 'fail'
 	ELSE 'pass'
 	END AS rule_status
 FROM
   aws_wafv2_web_acls;
+"""
+
+waf_global_webacl_not_empty = """
+insert into aws_policy_results
+SELECT
+	:1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+	'A WAF global web ACL should have at least one rule or rule group' as title,
+	account_id,
+	arn,
+	CASE
+	WHEN ARRAY_SIZE(rules) = 0 THEN 'fail'
+	ELSE 'pass'
+	END AS rule_status
+FROM
+  aws_waf_web_acls;
 """
