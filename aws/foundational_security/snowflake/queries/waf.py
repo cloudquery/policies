c2a55e9d5d5e880cfcd22d8b24175bef8123c100
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-08-01 02:12:16 +0300

    feat: Added queries of AWS WAF controls 2 to 10 * Added queries of AWS WAF controls 2 to 10 * Update aws/foundational_security/snowflake/queries/waf.py

--- aws/foundational_security/snowflake/queries/waf.py
+++ aws/foundational_security/snowflake/queries/waf.py
@@ -15,6 +15,23 @@ select
 from aws_waf_web_acls
 """
 
+WAFV2_WEBACL_NOT_EMPTY = """
+insert into aws_policy_results
+SELECT
+	:1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+	'A WAFV2 web ACL should have at least one rule or rule group' as title,
+	account_id,
+	arn,
+	CASE
+	WHEN ARRAY_SIZE(rules) = 0 THEN 'fail'
+	ELSE 'pass'
+	END AS rule_status
+FROM
+  aws_wafv2_web_acls;
+"""
+
 
 WAF_GLOBAL_WEBACL_NOT_EMPTY = """
 insert into aws_policy_results
@@ -26,14 +43,13 @@ SELECT
 	account_id,
 	arn,
 	CASE
-	WHEN ARRAY_SIZE(rules) = 0 THEN 'fail'
-	ELSE 'pass'
-	END AS rule_status
+        WHEN 
+        ARRAY_SIZE(rules) = 0 THEN 'fail'
+        ELSE 'pass'
+        END AS rule_status
 FROM
   aws_waf_web_acls;
 """
-
-
 
 WAF_GLOBAL_RULEGROUP_NOT_EMPTY = """
 -- Insert statement to add the result into aws_policy_results table
@@ -46,7 +62,7 @@ SELECT
 	account_id,
 	arn as resource_id,
 	case 
-		when 
+		WHEN 
 		rule_ids is null then 'fail'
 		else 'pass'
         end as status
@@ -61,14 +77,69 @@ SELECT
 	:1 as execution_time,
     :2 as framework,
     :3 as check_id,
-	'A WAF global rule group should have at least one rule' as title,
+	'A WAF global rule should have at least one condition' as title,
 	account_id,
 	arn as resource_id,
 	case 
-		when 
+		WHEN 
+        predicates is null 
+        or ARRAY_SIZE(predicates) = 0 then 'fail'
+		else 'pass'
+        end as status
+from
+  aws_waf_rules
+"""
+
+WAF_REGIONAL_WEBACL_NOT_EMPTY = """
+insert into aws_policy_results
+SELECT
+	:1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+	'A WAF Regional web ACL should have at least one rule or rule group' as title,
+	account_id,
+	arn as resource_id,
+	case  
+		WHEN ARRAY_SIZE(rules) = 0 THEN 'fail'
+		else 'pass'
+        end as status
+from
+  aws_wafregional_web_acls
+"""
+
+WAF_REGIONAL_RULEGROUP_NOT_EMPTY = """
+insert into aws_policy_results
+SELECT
+	:1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+	'A WAF Regional rule group should have at least one rule' as title,
+	account_id,
+	arn as resource_id,
+	case 
+		WHEN 
 		rule_ids is null then 'fail'
 		else 'pass'
         end as status
 from
-  aws_waf_rule_groups
+  aws_wafregional_rule_groups
+"""
+
+WAF_REGIONAL_RULE_NOT_EMPTY = """
+insert into aws_policy_results
+SELECT
+	:1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+	'A WAF Regional rule should have at least one condition' as title,
+	account_id,
+	arn as resource_id,
+	case 
+		WHEN 
+        predicates is null 
+        or ARRAY_SIZE(predicates) = 0 then 'fail'
+		else 'pass'
+        end as status
+from
+  aws_wafregional_rules
 """
