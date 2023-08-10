c3b6cb49a1a83b183eb8ca9a3a6e4e08d21cfc86
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-08-10 12:54:38 +0300

    feat: Added queries of Amazon RDS controls * Added queries of Amazon RDS controls 1 11 19-22 24 25 27 * Updated aws/foundational_security/snowflake/queries/rds.py

--- aws/foundational_security/snowflake/queries/rds.py
+++ aws/foundational_security/snowflake/queries/rds.py
@@ -1,3 +1,41 @@
+#RDS.1
+RDS_SNAPSHOTS_PUBLIC_PROHIBITED = """
+INSERT INTO aws_policy_results
+SELECT
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'RDS snapshot should be private' AS title,
+    account_id,
+    arn AS resource_id,
+    CASE 
+        WHEN a.value:AttributeName = 'restore' and ARRAY_CONTAINS('all'::variant, a.value:AttributeValues) THEN 'fail'
+        ELSE 'pass'
+    END AS status
+FROM
+    aws_rds_cluster_snapshots,
+    LATERAL FLATTEN(ATTRIBUTES) a
+
+UNION
+
+SELECT
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'RDS snapshot should be private' AS title,
+    account_id,
+    arn AS resource_id,
+    CASE 
+        WHEN a.value:AttributeName = 'restore' and ARRAY_CONTAINS('all'::variant, a.value:AttributeValues) THEN 'fail'
+        ELSE 'pass'
+    END AS status
+FROM
+    aws_rds_db_snapshots,
+    LATERAL FLATTEN(ATTRIBUTES) a;
+"""
+
+
+#RDS.2
 INSTANCES_SHOULD_PROHIBIT_PUBLIC_ACCESS = """
 insert into aws_policy_results
 select
@@ -11,7 +49,7 @@ select
 from aws_rds_instances
 """
 
-
+#RDS.8
 INSTANCES_SHOULD_HAVE_DELETION_PROTECTION_ENABLED = """
 insert into aws_policy_results
 select
@@ -25,6 +63,7 @@ select
 from aws_rds_instances
 """
 
+#RDS.4
 CLUSTER_SNAPSHOTS_AND_DATABASE_SNAPSHOTS_SHOULD_BE_ENCRYPTED_AT_REST = """
 insert into aws_policy_results
 (
@@ -52,6 +91,7 @@ union
 )
 """
 
+#RDS.3
 INSTANCES_SHOULD_HAVE_ECNRYPTION_AT_REST_ENABLED = """
 insert into aws_policy_results
 select
@@ -64,6 +104,7 @@ select
     case when storage_encrypted != TRUE then 'fail' else 'pass' end as status
 from aws_rds_instances
 """
+#RDS.5
 INSTANCES_SHOULD_BE_CONFIGURED_WITH_MULTIPLE_AZS = """
 insert into aws_policy_results
 select
@@ -77,6 +118,7 @@ select
 from aws_rds_instances
 """
 
+#RDS.6
 ENHANCED_MONITORING_SHOULD_BE_CONFIGURED_FOR_INSTANCES_AND_CLUSTERS = """
 insert into aws_policy_results
 select
@@ -90,6 +132,7 @@ select
 from aws_rds_instances
 """
 
+#RDS.7
 CLUSTERS_SHOULD_HAVE_DELETION_PROTECTION_ENABLED = """
 insert into aws_policy_results
 select
@@ -103,6 +146,7 @@ select
 from aws_rds_clusters
 """
 
+#RDS.9
 DATABASE_LOGGING_SHOULD_BE_ENABLED = """
 INSERT INTO aws_policy_results
 SELECT
@@ -131,6 +175,26 @@ SELECT
 FROM aws_rds_instances;
 """
 
+#RDS.11
+DB_INSTANCE_BACKUP_ENABLED = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'RDS instances should have automatic backups enabled' as title,
+    account_id,
+    arn AS resource_id,
+    CASE 
+        WHEN backup_retention_period IS NULL
+            OR backup_retention_period < 7 THEN 'fail'
+        ELSE 'pass'
+    END AS status
+FROM
+    aws_rds_instances
+"""
+
+#RDS.12
 IAM_AUTHENTICATION_SHOULD_BE_CONFIGURED_FOR_RDS_CLUSTERS = """
 insert into aws_policy_results
 select
@@ -144,6 +208,7 @@ select
 from aws_rds_clusters
 """
 
+#RDS.10
 IAM_AUTHENTICATION_SHOULD_BE_CONFIGURED_FOR_RDS_INSTANCES = """
 insert into aws_policy_results
 select
@@ -157,6 +222,7 @@ select
 from aws_rds_instances
 """
 
+#RDS.13
 RDS_AUTOMATIC_MINOR_VERSION_UPGRADES_SHOULD_BE_ENABLED = """
 insert into aws_policy_results
 select
@@ -170,6 +236,7 @@ select
 from aws_rds_instances
 """
 
+#RDS.14
 AMAZON_AURORA_CLUSTERS_SHOULD_HAVE_BACKTRACKING_ENABLED = """
 insert into aws_policy_results
 select
@@ -185,6 +252,7 @@ where
     engine in ('aurora', 'aurora-mysql', 'mysql')
 """
 
+#RDS.15
 CLUSTERS_SHOULD_BE_CONFIGURED_FOR_MULTIPLE_AVAILABILITY_ZONES = """
 insert into aws_policy_results
 select
@@ -198,6 +266,7 @@ select
 from aws_rds_clusters
 """
 
+#RDS.16
 CLUSTERS_SHOULD_BE_CONFIGURED_TO_COPY_TAGS_TO_SNAPSHOTS = """
 insert into aws_policy_results
 select
@@ -211,6 +280,7 @@ select
 from aws_rds_clusters
 """
 
+#RDS.17
 INSTANCES_SHOULD_BE_CONFIGURED_TO_COPY_TAGS_TO_SNAPSHOTS = """
 insert into aws_policy_results
 select
@@ -224,6 +294,7 @@ select
 from aws_rds_instances
 """
 
+#RDS.18
 INSTANCES_SHOULD_BE_DEPLOYED_IN_A_VPC = """
 insert into aws_policy_results
 select
@@ -236,7 +307,95 @@ select
     case when db_subnet_group:VpcId is null then 'fail' else 'pass' end as status
 from aws_rds_instances
 """
+#RDS.19
+RDS_CLUSTER_EVENT_NOTIFICATIONS_CONFIGURED = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'An RDS event notifications subscription should be configured for critical cluster events' as title,
+    account_id,
+    SOURCE_ARN AS resource_id,
+    CASE 
+        WHEN 
+            ARRAY_CONTAINS('maintenance'::variant, EVENT_CATEGORIES) AND
+            ARRAY_CONTAINS('failure'::variant, EVENT_CATEGORIES)
+            THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_rds_events
+WHERE source_type = 'db-cluster'
+"""
 
+#RDS.20
+RDS_INSTANCE_EVENT_NOTIFICATIONS_CONFIGURED = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'An RDS event notifications subscription should be configured for critical database instance events' as title,
+    account_id,
+    SOURCE_ARN AS resource_id,
+    CASE 
+        WHEN   
+            ARRAY_CONTAINS('maintenance'::variant, EVENT_CATEGORIES) AND
+            ARRAY_CONTAINS('configuration change'::variant, EVENT_CATEGORIES) AND
+            ARRAY_CONTAINS('failure'::variant, EVENT_CATEGORIES)
+            THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_rds_events
+WHERE
+    source_type = 'db-instance'
+"""
+
+#RDS.21
+RDS_PG_EVENT_NOTIFICATIONS_CONFIGURED = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'An RDS event notifications subscription should be configured for critical database parameter group events' as title,
+    account_id,
+    SOURCE_ARN AS resource_id,
+    CASE 
+        WHEN 
+            ARRAY_CONTAINS('configuration change'::variant, EVENT_CATEGORIES) THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_rds_events
+where source_type = 'db-parameter-group'
+"""
+
+#RDS.22
+RDS_SG_EVENT_NOTIFICATIONS_CONFIGURED = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'An RDS event notifications subscription should be configured for critical database security group events' as title,
+    account_id,
+    SOURCE_ARN AS resource_id,
+    CASE 
+        WHEN 
+            ARRAY_CONTAINS('configuration change'::variant, EVENT_CATEGORIES) AND
+            ARRAY_CONTAINS('failure'::variant, EVENT_CATEGORIES)
+            THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_rds_events
+where source_type = 'db-security-group'
+"""
+
+#RDS.23
 DATABASES_AND_CLUSTERS_SHOULD_NOT_USE_DATABASE_ENGINE_DEFAULT_PORT = """
 INSERT INTO aws_policy_results
 (
@@ -273,3 +432,58 @@ UNION ALL
     FROM aws_rds_instances
 );
 """
+
+#RDS.24
+RDS_CLUSTER_DEFAULT_ADMIN_CHECK = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'RDS Database clusters should use a custom administrator username' as title,
+    account_id,
+    arn AS resource_id,
+    CASE
+        WHEN master_username NOT IN ('admin', 'root', 'administrator', 'master', 'sa', 'awsuser') THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_rds_clusters;
+"""
+
+#RDS.25
+RDS_INSTANCE_DEFAULT_ADMIN_CHECK = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'RDS database instances should use a custom administrator username' as title,
+    account_id,
+    arn AS resource_id,
+    CASE
+        WHEN master_username NOT IN ('admin', 'root', 'administrator', 'master', 'sa', 'awsuser') THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_rds_instances;
+"""
+
+#RDS.27
+RDS_CLUSTER_ENCRYPTED_AT_REST = """
+insert into aws_policy_results
+select
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'RDS DB clusters should be encrypted at rest' as title,
+    account_id,
+    arn AS resource_id,
+    CASE
+        WHEN storage_encrypted THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_rds_clusters;
+"""
+
