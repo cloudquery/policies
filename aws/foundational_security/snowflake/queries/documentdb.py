25447a68953bde752ef561ea8b5546a6f2a98c56
john-s58 <jonathansarig58@gmail.com>
2023-08-20 10:59:04 +0300

    added documentdb queries

---
+++ aws/foundational_security/snowflake/queries/documentdb.py
@@ -0,0 +1,34 @@
+
+#DocumentDB.1
+CLUSTERS_SHOULD_BE_ENCRYPTED_AT_REST = """
+insert into aws_policy_results
+SELECT
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'Amazon DocumentDB clusters should be encrypted at rest' as title,
+    account_id,
+    arn as resource_id,
+    CASE
+        WHEN storage_encrypted = false THEN 'fail'
+        ELSE 'pass'
+    END as status
+FROM aws_docdb_clusters
+"""
+
+#DocumentDB.2
+CLUSTERS_SHOULD_HAVE_7_DAYS_BACKUP_RETENTION = """
+insert into aws_policy_results
+SELECT 
+    :1 as execution_time,
+    :2 as framework,
+    :3 as check_id,
+    'Amazon DocumentDB clusters should have an adequate backup retention period' as title,
+    account_id,
+    arn as resource_id,
+    CASE
+        WHEN WHEN backup_retention_period < 7 THEN 'fail'
+        ELSE 'pass'
+    END as status
+FROM aws_docdb_clusters
+"""
