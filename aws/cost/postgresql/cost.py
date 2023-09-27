d228f1f0243bb109e5df14a07ed909fafa2bbc2f
Jason Kao <100613312+jsonpr@users.noreply.github.com>
2023-09-27 11:49:58 -0400

    Update spacing and capitalization

--- aws/cost/postgresql/cost.py
+++ aws/cost/postgresql/cost.py
@@ -72,10 +72,10 @@ COMPUTE_OPTIMIZER_RESULTS = """
 CREATE OR REPLACE VIEW compute_optimizer_results as
 SELECT *
 FROM (SELECT * 
-From aws_computeoptimizer_ec2_instance_recommendations
-FULL JOIN aws_computeoptimizer_lambda_function_recommendations on true
-FULL JOIN aws_computeoptimizer_ebs_volume_recommendations on true
-FULL JOIN aws_computeoptimizer_ecs_service_recommendations on true
-FULL JOIN aws_computeoptimizer_autoscaling_group_recommendations on true
-)
+  FROM aws_computeoptimizer_ec2_instance_recommendations
+  FULL JOIN aws_computeoptimizer_lambda_function_recommendations on true
+  FULL JOIN aws_computeoptimizer_ebs_volume_recommendations on true
+  FULL JOIN aws_computeoptimizer_ecs_service_recommendations on true
+  FULL JOIN aws_computeoptimizer_autoscaling_group_recommendations on true
+) as compute_opt
 """
