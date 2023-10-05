621fa80c8c5d860a08c0a38ecdc265d8bd5020df
Jason Kao <100613312+jsonpr@users.noreply.github.com>
2023-10-05 07:28:44 -0400

    Update Compute Optimizer Query

--- aws/cost/postgresql/cost.py
+++ aws/cost/postgresql/cost.py
@@ -71,11 +71,20 @@ WHERE vols.volume_type = 'gp2'
 COMPUTE_OPTIMIZER_RESULTS = """
 CREATE OR REPLACE VIEW compute_optimizer_results as
 SELECT *
-FROM (SELECT * 
-  FROM aws_computeoptimizer_ec2_instance_recommendations
-  FULL JOIN aws_computeoptimizer_lambda_function_recommendations on true
-  FULL JOIN aws_computeoptimizer_ebs_volume_recommendations on true
-  FULL JOIN aws_computeoptimizer_ecs_service_recommendations on true
-  FULL JOIN aws_computeoptimizer_autoscaling_group_recommendations on true
+FROM (  
+	SELECT account_id,  instance_arn as arn, recommendation_options as roptions
+  	FROM aws_computeoptimizer_ec2_instance_recommendations
+  	UNION
+  	SELECT account_id,  function_arn as arn, memory_size_recommendation_options as roptions 
+  	FROM aws_computeoptimizer_lambda_function_recommendations
+  	UNION
+  	SELECT account_id, volume_arn as arn, volume_recommendation_options as roptions 
+  	FROM aws_computeoptimizer_ebs_volume_recommendations 
+  	UNION
+	SELECT account_id, service_arn as arn, service_recommendation_options as roptions
+	FROM aws_computeoptimizer_ecs_service_recommendations
+  	UNION
+	SELECT account_id, auto_scaling_group_arn as arn,  recommendation_options as roptions
+	from aws_computeoptimizer_autoscaling_group_recommendations
 ) as compute_opt
 """
