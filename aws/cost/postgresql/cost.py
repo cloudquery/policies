05496a0b0bac627c80e94a06d14d88526cc7799b
Jason Kao <100613312+jsonpr@users.noreply.github.com>
2023-10-05 10:33:00 -0400

    Add Trusted Advisor Information

--- aws/cost/postgresql/cost.py
+++ aws/cost/postgresql/cost.py
@@ -88,3 +88,14 @@ FROM (
 	from aws_computeoptimizer_autoscaling_group_recommendations
 ) as compute_opt
 """
+
+#AWS Trusted Advisor Results
+TRUSTED_ADVISIOR_RESULTS = """
+CREATE OR REPLACE VIEW trusted_advisor_results as
+	SELECT checks.account_id, category, description, id, name, flagged_resources, resources_summary, status, timestamp
+	FROM aws_support_trusted_advisor_checks as checks
+	LEFT JOIN aws_support_trusted_advisor_check_results as results
+	ON checks.id = results.check_id
+	WHERE category='cost_optimizing' and checks.language_code='en' 
+) as trust_adv
+"""
