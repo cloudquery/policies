de0fc3040755a88a655c4aa70174a1d33b9b63f5
Jonathan Sarig <73023114+john-s58@users.noreply.github.com>
2023-10-19 19:46:44 +0300

    feat: Added queries for sections 5.2 to 5.7 (#94)
    * added queries for sections 5.2 to 5.7
    
    * fixed name typos
    
    * fixed naming typos in query folder
    
    * Update policy_cis_v1_7.sql
    
    ---------
    
    Co-authored-by: Jason Kao <100613312+jsonpr@users.noreply.github.com>

---
+++ k8s/queries_cis_v1_7_0/network_policies_and_cni_5_3_queries.sql
@@ -0,0 +1,33 @@
+-- 5.3.1 - a simple check there is a network policy
+-- SELECT *
+-- FROM k8s_networking_network_policies;
+
+-- 5.3.2
+ \echo "pod_security_standards_5.3.2"
+
+ INSERT INTO k8s_policy_results (resource_id, execution_time, framework, check_id, title, context, namespace,
+                                 resource_name, status)
+SELECT 
+    uid                              AS resource_id,
+    :'execution_time'::timestamp     AS execution_time,
+    :'framework'                     AS framework,
+    'pod_security_standards_5.3.2'                      AS check_id,
+    'Ensure that all Namespaces have Network Policies defined' AS title,
+    context                          AS context,
+    namespace                        AS namespace,
+    name                             AS resource_name,
+    CASE
+        WHEN (
+        SELECT
+          count(*)
+        FROM
+          k8s_networking_network_policies np
+        WHERE
+          np.namespace = n.name
+      )
+      = 0
+      THEN 'fail'
+      ELSE 'pass'
+    END as status
+FROM 
+    k8s_core_namespaces n
