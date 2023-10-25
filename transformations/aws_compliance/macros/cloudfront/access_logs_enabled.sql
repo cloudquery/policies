56a88e293fcd8de311b42c4281e4d7741181d223
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-10-25 17:53:52 -0400

    feat: Reorder things and move to cross platform queries

---
+++ transformations/aws_compliance/macros/cloudfront/access_logs_enabled.sql
@@ -0,0 +1,14 @@
+{% macro access_logs_enabled(framework, check_id) %}
+select
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'CloudFront distributions should have logging enabled' as title,
+    account_id,
+    arn as resource_id,
+    case
+        when {{ json_parse("distribution_config", ["Logging", "Enabled"]) }} is distinct from true then 'fail'
+        -- when (distribution_config:Logging:Enabled)::boolean is distinct from true then 'fail'
+        else 'pass'
+    end as status
+from aws_cloudfront_distributions
+{% endmacro %}
