e058c61db1c1ae5e9e29f20256cbb448bb76886f
Marcel <144216124+maaarcelino@users.noreply.github.com>
2023-10-31 14:01:08 +0100

    chore: Remove to_migrate directory as k8s was already migrated (#171)
    Removing stale directory as the policies were migrated in https://github.com/cloudquery/policies-premium/pull/165

--- to_migrate/k8s/cis_v1_7_0/pod_security_standards_5_2.sql
+++
@@ -1,6 +0,0 @@
-\echo "Executing K8S Pod Security Standards CIS v1.7.0"
-
-\echo "Policies - Pod Security Standards"
-
-\echo "Queries - Pod Security Standards"
- \ir ../queries_cis_v1_7_0/pod_security_standards_5_2_queries.sql
