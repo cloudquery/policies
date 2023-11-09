3749030990f662acde611fa790c1f32ad60cb1fc
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-11-09 16:14:12 -0500

    chore: Split k8s to free and pro

---
+++ transformations/k8s_compliance/models/free/k8s_compliance__cis_v1_7_free.sql
@@ -0,0 +1,21 @@
+with
+    aggregated as (
+        ({{ api_server_1_2_1('Kubernetes CIS v1.7.0','api_server_1_2_1') }})
+                union
+        ({{ api_server_1_2_2('Kubernetes CIS v1.7.0','api_server_1_2_2') }})
+                union
+        ({{ api_server_1_2_3('Kubernetes CIS v1.7.0','api_server_1_2_3') }})
+                union
+        ({{ api_server_1_2_4('Kubernetes CIS v1.7.0','api_server_1_2_4') }})
+                union
+        ({{ api_server_1_2_5('Kubernetes CIS v1.7.0','api_server_1_2_5') }})
+                union
+        ({{ api_server_1_2_6('Kubernetes CIS v1.7.0','api_server_1_2_6') }})
+                union
+        ({{ api_server_1_2_7('Kubernetes CIS v1.7.0','api_server_1_2_7') }})
+                union
+        ({{ api_server_1_2_8('Kubernetes CIS v1.7.0','api_server_1_2_8') }})
+    )
+select 
+*
+from aggregated
