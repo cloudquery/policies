12c80d1fe6fd851a4eccf46fc47eea847bb38a39
Jason Kao <jason@cloudquery.io>
2023-11-12 14:28:05 -0500

    update backup resource macro

--- transformations/aws_backup/macros/aws_data_resilience__backup_resource.sql
+++ transformations/aws_backup/macros/aws_data_resilience__backup_resource.sql
@@ -1,6 +1,5 @@
 {% set resource_tables = [{"dynamodb", "aws_dynamodb_tables"}, {"s3", "aws_s3_buckets"}, {"ec2", "aws_ec2_instances"}] %}
 
-
 {% macro backup_resource(resource_type) %}
     SELECT {{resource_type}}.account_id, {{resource_type}}.arn as resource_arn, tags, last_backup_time, 'DynamoDB' as resource_type
     FROM {{resource_type}}
