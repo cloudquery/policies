c3e7b2f44356160fd8859175a6c9b2381d9b05c0
Jason Kao <jason@cloudquery.io>
2023-11-12 15:07:41 -0500

    fix hardcoded dynamodb

--- transformations/aws_backup/macros/aws_data_resilience__backup_resource.sql
+++ transformations/aws_backup/macros/aws_data_resilience__backup_resource.sql
@@ -1,10 +1,10 @@
 {% set resource_tables = [{"dynamodb", "aws_dynamodb_tables"}, {"s3", "aws_s3_buckets"}, {"ec2", "aws_ec2_instances"}] %}
 
-{% macro backup_resource(resource_type) %}
-    SELECT {{resource_type}}.account_id, {{resource_type}}.arn as resource_arn, tags, last_backup_time, 'DynamoDB' as resource_type
-    FROM {{resource_type}}
+{% macro backup_resource(resource_table, resource_type) %}
+    SELECT {{resource_table}}.account_id, {{resource_table}}.arn as resource_arn, tags, last_backup_time, '{{resource_type}}' as resource_type
+    FROM {{resource_table}}
     LEFT JOIN aws_backup_protected_resources
-    ON aws_backup_protected_resources.resource_arn = {{resource_type}}.arn
+    ON aws_backup_protected_resources.resource_arn = {{resource_table}}.arn
 {% endmacro %}
 
 
--- transformations/aws_backup/models/aws_data_resilience.sql
+++ transformations/aws_backup/models/aws_data_resilience.sql
@@ -1,10 +1,10 @@
 with
     backup_eligible_resources as (
-        ({{backup_resource('aws_dynamodb_tables')}})
+        ({{backup_resource('aws_dynamodb_tables', 'DynamoDB')}})
         union
-        ({{backup_resource('aws_ec2_instances')}})
+        ({{backup_resource('aws_ec2_instances', 'EC2')}})
         union
-        ({{backup_resource('aws_s3_buckets')}})
+        ({{backup_resource('aws_s3_buckets', 'S3')}})
  )
 select *
 from backup_eligible_resources
