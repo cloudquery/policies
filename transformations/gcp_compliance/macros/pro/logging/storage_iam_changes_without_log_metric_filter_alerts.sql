b2238206fa0154bab459a46e57353fab5828994e
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-11-20 15:16:35 +0200

    Added section Logging

--- transformations/gcp_compliance/macros/pro/logging/storage_iam_changes_without_log_metric_filter_alerts.sql
+++ transformations/gcp_compliance/macros/pro/logging/storage_iam_changes_without_log_metric_filter_alerts.sql
@@ -41,3 +41,21 @@ select
                 END AS status
     FROM gcp_logging_metrics
 {% endmacro %}
+
+{% macro bigquery__logging_storage_iam_changes_without_log_metric_filter_alerts(framework, check_id) %}
+select DISTINCT 
+                filter                                                                    AS resource_id,
+                _cq_sync_time As sync_time,
+                '{{framework}}' As framework,
+                '{{check_id}}' As check_id,                                                                         
+                'Ensure that the log metric filter and alerts exist for Cloud Storage IAM permission changes (Automated)' AS title,
+                project_id                                                                AS project_id,
+                CASE
+                WHEN
+                            disabled = FALSE
+                        AND REGEXP_CONTAINS(filter, r'\s*resource.type\s*=\s*gcs_bucket\s*AND\s*protoPayload.methodName\s*=\s*"storage.setIamPermissions"\s*')
+                THEN 'fail'
+                ELSE 'pass'
+                END AS status
+    FROM {{ full_table_name("gcp_logging_metrics") }}
+{% endmacro %}
