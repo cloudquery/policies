63317bd233d7e4bdadcc47eb422d47e7f81c999b
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-11-20 15:58:19 +0200

    Added section Dns

--- transformations/gcp_compliance/macros/pro/dns/key_signing_with_rsasha1.sql
+++ transformations/gcp_compliance/macros/pro/dns/key_signing_with_rsasha1.sql
@@ -42,3 +42,23 @@ select
     FROM gcp_dns_managed_zones gdmz, 
     LATERAL FLATTEN(input => gdmz.dnssec_config:defaultKeySpecs) AS gdmzdcdks
 {% endmacro %}
+
+{% macro bigquery__dns_key_signing_with_rsasha1(framework, check_id) %}
+select
+    DISTINCT 
+                CAST(gdmz.id AS STRING)                                                                           AS resource_id,
+                gdmz._cq_sync_time As sync_time,
+                '{{framework}}' As framework,
+                '{{check_id}}' As check_id,                                                                         
+                'Ensure that DNSSEC is enabled for Cloud DNS (Automated)' AS title,
+                gdmz.project_id                                                                             AS project_id,
+                CASE
+                    WHEN
+                                JSON_VALUE(gdmzdcdks.keyType) = 'keySigning'
+                            AND JSON_VALUE(gdmzdcdks.algorithm) = 'rsasha1'
+                        THEN 'fail'
+                    ELSE 'pass'
+                    END AS status
+    FROM {{ full_table_name("gcp_dns_managed_zones") }} gdmz,
+    UNNEST(JSON_QUERY_ARRAY(gdmz.dnssec_config.defaultKeySpecs)) AS gdmzdcdks 
+{% endmacro %}
