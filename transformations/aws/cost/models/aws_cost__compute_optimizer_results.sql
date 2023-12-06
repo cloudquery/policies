b225f105afb5a4bdf00c03a43e2762843cd3aaf2
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-12-06 11:09:52 +0200

    feat: Added queries for aws cost - compute optimizer, trusted advisior (#331)

--- transformations/aws/cost/models/aws_cost__compute_optimizer_results.sql
+++ transformations/aws/cost/models/aws_cost__compute_optimizer_results.sql
@@ -1,8 +1,17 @@
 SELECT *
-FROM (SELECT * 
-  FROM aws_computeoptimizer_ec2_instance_recommendations
-  FULL JOIN aws_computeoptimizer_lambda_function_recommendations on true
-  FULL JOIN aws_computeoptimizer_ebs_volume_recommendations on true
-  FULL JOIN aws_computeoptimizer_ecs_service_recommendations on true
-  FULL JOIN aws_computeoptimizer_autoscaling_group_recommendations on true
+FROM (  
+	SELECT account_id,  instance_arn as arn, recommendation_options as roptions
+  	FROM aws_computeoptimizer_ec2_instance_recommendations
+  	UNION
+  	SELECT account_id,  function_arn as arn, memory_size_recommendation_options as roptions 
+  	FROM aws_computeoptimizer_lambda_function_recommendations
+  	UNION
+  	SELECT account_id, volume_arn as arn, volume_recommendation_options as roptions 
+  	FROM aws_computeoptimizer_ebs_volume_recommendations 
+  	UNION
+	SELECT account_id, service_arn as arn, service_recommendation_options as roptions
+	FROM aws_computeoptimizer_ecs_service_recommendations
+  	UNION
+	SELECT account_id, auto_scaling_group_arn as arn,  recommendation_options as roptions
+	from aws_computeoptimizer_autoscaling_group_recommendations
 ) as compute_opt
