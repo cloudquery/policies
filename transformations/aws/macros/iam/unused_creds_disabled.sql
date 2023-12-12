bc1c82635ee3c77ad70c9a7a7991d22d553ee8c8
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-12-12 11:14:15 +0200

    feat: Added queries for free AWS Compliance - Snowflake

--- transformations/aws/macros/iam/unused_creds_disabled.sql
+++ transformations/aws/macros/iam/unused_creds_disabled.sql
@@ -37,3 +37,20 @@ select
 from {{ full_table_name("aws_iam_credential_reports") }} r
 left join {{ full_table_name("aws_iam_user_access_keys") }} k on k.user_arn = r.arn
 {% endmacro %}
+
+{% macro snowflake__unused_creds_disabled(framework, check_id) %}
+select
+  '{{framework}}' as framework,
+  '{{check_id}}' as check_id,
+  'Ensure credentials unused for 90 days or greater are disabled (Scored)' as title,
+  split_part(r.arn, ':', 5) as account_id,
+  r.arn,
+  case when
+      (r.password_status IN ('TRUE', 'true') and r.password_last_used < (CURRENT_TIMESTAMP() - INTERVAL '90 DAY')
+        or (k.last_used < (CURRENT_TIMESTAMP() - INTERVAL '90 DAY')))
+      then 'fail'
+      else 'pass'
+  end
+from aws_iam_credential_reports r
+left join aws_iam_user_access_keys k on k.user_arn = r.arn
+{% endmacro %}
