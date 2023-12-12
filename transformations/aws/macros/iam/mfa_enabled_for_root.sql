1d0a10d63ffd03b6f834cb8302cf761f24ba56fd
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-12-12 11:53:58 +0200

    feat: Added queries for AWS CIS for Snowflake - Premium

--- transformations/aws/macros/iam/mfa_enabled_for_root.sql
+++ transformations/aws/macros/iam/mfa_enabled_for_root.sql
@@ -31,3 +31,17 @@ select
   end as status
 from {{ full_table_name("aws_iam_credential_reports") }}
 {% endmacro %}
+
+{% macro snowflake__mfa_enabled_for_root(framework, check_id) %}
+select
+  '{{framework}}' as framework,
+  '{{check_id}}' as check_id,
+  'Ensure MFA is enabled for the "root" account' as title,
+  split_part(arn, ':', 5) as account_id,
+  arn as resource_id,
+  case
+    when user = '<root_account>' and not mfa_active then 'fail' -- TODO check
+    when user = '<root_account>' and mfa_active then 'pass'
+  end as status
+from aws_iam_credential_reports
+{% endmacro %}
--- transformations/aws/macros/iam/password_policy_min_length.sql
+++ transformations/aws/macros/iam/password_policy_min_length.sql
@@ -35,3 +35,19 @@ select
 from
     {{ full_table_name("aws_iam_password_policies") }}
 {% endmacro %}
+
+{% macro snowflake__password_policy_min_length(framework, check_id) %}
+select
+  '{{framework}}' as framework,
+  '{{check_id}}' as check_id,
+  'Ensure IAM password policy requires minimum length of 14 or greater' as title,
+  account_id,
+  account_id,
+  case when
+    (minimum_password_length < 14) or policy_exists = FALSE
+    then 'fail'
+    else 'pass'
+  end
+from
+    aws_iam_password_policies
+{% endmacro %}
