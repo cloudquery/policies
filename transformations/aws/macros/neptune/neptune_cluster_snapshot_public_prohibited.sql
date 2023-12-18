6a0e29c5851f5d7244e4eca1dca277050d97e935
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-12-18 18:31:58 +0200

    Upated query neptune_cluster_snapshot_public_prohibited

--- transformations/aws/macros/neptune/neptune_cluster_snapshot_public_prohibited.sql
+++ transformations/aws/macros/neptune/neptune_cluster_snapshot_public_prohibited.sql
@@ -5,20 +5,20 @@
 {% macro default__neptune_cluster_snapshot_public_prohibited(framework, check_id) %}{% endmacro %}
 
 {% macro postgres__neptune_cluster_snapshot_public_prohibited(framework, check_id) %}
-select
-    '{{framework}}' As framework,
-    '{{check_id}}' As check_id,
-    'Neptune DB cluster snapshots should not be public' as title,
+SELECT
+    '{{framework}}' AS framework,
+    '{{check_id}}' AS check_id,
+    'Neptune DB cluster snapshots should not be public' AS title,
     account_id,
-    arn as resource_id,
-    case when
-    attributes[0] ->> 'AttributeName' = 'restore' 
-	and attributes[0] -> 'AttributeValues' ? 'all'
-	then 'fail'
-    else 'pass'
-    end as status
-from 
-    aws_neptune_cluster_snapshots
+    arn AS resource_id,
+    CASE
+        WHEN a ->> 'AttributeName' = 'restore' AND a -> 'AttributeValues' ? 'all'
+        THEN 'fail'
+        ELSE 'pass'
+    END AS status
+FROM 
+    aws_neptune_cluster_snapshots,
+    JSONB_ARRAY_ELEMENTS(attributes) AS a
 {% endmacro %}
 
 {% macro snowflake__neptune_cluster_snapshot_public_prohibited(framework, check_id) %}
