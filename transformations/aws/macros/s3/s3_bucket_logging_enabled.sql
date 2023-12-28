4e5cc6988c009af5f9cfca85412b1a01f428b7fc
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-12-28 13:12:18 +0200

    check query

--- transformations/aws/macros/s3/s3_bucket_logging_enabled.sql
+++ transformations/aws/macros/s3/s3_bucket_logging_enabled.sql
@@ -3,14 +3,12 @@ select
     '{{framework}}' As framework,
     '{{check_id}}' As check_id,
     'S3 bucket server access logging should be enabled' AS title,
-    b.account_id,
-    b.arn AS resource_id,
+    account_id,
+    arn AS resource_id,
     CASE
-        when bl.logging_enabled:TargetBucket IS NOT NULL
+        when logging_target_bucket IS NOT NULL
         THEN 'pass' ELSE 'fail'
     END AS status
 FROM
-    aws_s3_buckets as b
-LEFT JOIN
-    aws_s3_bucket_loggings as bl on bl.bucket_arn = b.arn
+    aws_s3_buckets
 {% endmacro %}
