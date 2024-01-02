0b92173575ddac160226e4e7ab844ae16ca3a502
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-01-02 11:57:46 +0200

    Changed queries

--- transformations/aws/macros/s3/s3_bucket_logging_enabled.sql
+++ transformations/aws/macros/s3/s3_bucket_logging_enabled.sql
@@ -9,14 +9,16 @@ select
     '{{framework}}' As framework,
     '{{check_id}}' As check_id,
     'S3 bucket server access logging should be enabled' AS title,
-    account_id,
-    arn AS resource_id,
+    b.account_id,
+    b.arn AS resource_id,
     CASE
-        when logging_target_bucket IS NOT NULL
+        when bl.logging_enabled -> 'TargetBucket' IS NOT NULL
         THEN 'pass' ELSE 'fail'
     END AS status
 FROM
-    aws_s3_buckets
+    aws_s3_buckets as b
+LEFT JOIN
+    aws_s3_bucket_loggings as bl on bl.bucket_arn = b.arn
 {% endmacro %}
 
 {% macro snowflake__s3_bucket_logging_enabled(framework, check_id) %}
