900da2935416ecd9418f5f4842565405240dc9b7
john-s58 <jonathansarig58@gmail.com>
2024-01-21 14:04:52 +0200

    fixed query to handle multiple method settings

--- transformations/aws/macros/apigateway/api_gateway_method_settings.sql
+++ transformations/aws/macros/apigateway/api_gateway_method_settings.sql
@@ -5,45 +5,47 @@
 {% macro default__api_gateway_method_settings() %}{% endmacro %}
 
 {% macro postgres__api_gateway_method_settings() %}
-with apigateway_rest_api_stages as(
-  select
+WITH apigateway_rest_api_stages AS (
+  SELECT
     *,
-    method_settings::text = '{}'::text as is_empty_method_settings
-  from aws_apigateway_rest_api_stages
+    method_settings::text = '{}'::text AS is_empty_method_settings
+  FROM aws_apigateway_rest_api_stages
 )
-select
-     s.arn,
-     s.rest_api_arn,
-	 s.stage_name,
-	 s.tracing_enabled as stage_data_trace_enabled,
-     s.cache_cluster_enabled as stage_caching_enabled,
-     s.web_acl_arn as waf,
-     s.client_certificate_id as cert,
-case when (s.is_empty_method_settings = false)
-        then (select key from JSONB_EACH_TEXT(s.method_settings))
-        else '/*/'
-    end as method,
-    case when (s.is_empty_method_settings = false)
-        then (select (value::JSON -> 'DataTraceEnabled')::TEXT::BOOLEAN from JSONB_EACH_TEXT(s.method_settings))
-        else false
-    end as data_trace_enabled,
-    case when (s.is_empty_method_settings = false)
-        then (select (value::JSON -> 'CachingEnabled')::TEXT::BOOLEAN from JSONB_EACH_TEXT(s.method_settings))
-        else false
-    end as caching_enabled,
-    case when (s.is_empty_method_settings = false)
-        then (select (value::JSON -> 'CacheDataEncrypted')::TEXT::BOOLEAN from JSONB_EACH_TEXT(s.method_settings))
-        else false
-    end as cache_data_encrypted,
-    case when (s.is_empty_method_settings = false)
-        then (select (value::JSON -> 'LoggingLevel')::TEXT from JSONB_EACH_TEXT(s.method_settings))
-        else '"OFF"'
-   end as logging_level,
-     r.account_id
-
-from apigateway_rest_api_stages s, aws_apigateway_rest_apis r
- where s.rest_api_arn=r.arn
-
+SELECT
+    s.arn,
+    s.rest_api_arn,
+    s.stage_name,
+    s.tracing_enabled AS stage_data_trace_enabled,
+    s.cache_cluster_enabled AS stage_caching_enabled,
+    s.web_acl_arn AS waf,
+    s.client_certificate_id AS cert,
+    CASE
+    WHEN s.is_empty_method_settings THEN '/*/'
+    ELSE methods.key
+    END as method,
+    CASE
+    WHEN s.is_empty_method_settings THEN false
+    ELSE (methods.value::JSON -> 'DataTraceEnabled')::TEXT::BOOLEAN
+    END as data_trace_enabled,
+    CASE
+    WHEN s.is_empty_method_settings THEN false
+    ELSE (methods.value::JSON -> 'CachingEnabled')::TEXT::BOOLEAN
+    END as caching_enabled,
+    CASE
+    WHEN s.is_empty_method_settings THEN false
+    ELSE (methods.value::JSON -> 'CacheDataEncrypted')::TEXT::BOOLEAN
+    END as cache_data_encrypted,
+    CASE
+    WHEN s.is_empty_method_settings THEN '"OFF"'
+    ELSE (methods.value::JSON -> 'LoggingLevel')::TEXT
+    END as logging_level,
+    r.account_id
+FROM
+    apigateway_rest_api_stages s
+JOIN
+    aws_apigateway_rest_apis r ON s.rest_api_arn = r.arn
+LEFT JOIN LATERAL
+    JSONB_EACH_TEXT(s.method_settings) AS methods ON s.is_empty_method_settings = false
 {% endmacro %}
 
 
