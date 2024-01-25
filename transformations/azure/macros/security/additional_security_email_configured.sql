44d332feee1a93771645fbfca393ee92bc6ce367
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-01-25 14:00:12 -0500

    more progress on security, added macros for email alert configs

--- transformations/azure/macros/security/additional_security_email_configured.sql
+++ transformations/azure/macros/security/additional_security_email_configured.sql
@@ -2,16 +2,49 @@
   {{ return(adapter.dispatch('security_additional_security_email_configured')(framework, check_id)) }}
 {% endmacro %}
 
+{% macro default__additional_security_email_configured(framework, check_id) %}{% endmacro %}
+
+{% macro postgres__additional_security_email_configured(framework, check_id) %}
 SELECT
   id,
   '{{framework}}' As framework,
   '{{check_id}}' As check_id,
-  'Ensure "Additional email addresses" is configured with a security contact email (Automated)' as title,
+  'Ensure "Additional email addresses" is Configured with a Security Contact Email (Automated)' as title,
   subscription_id,
   case
-    when email IS NOT NULL
-      AND email != ''
+    when properties ->> 'email' is not null
     then 'pass' else 'fail'
   end
-FROM azure_security_contacts
+FROM azure_security_contacts asc
+where "name" = "default"
+{% endmacro %}
+
+{% macro snowflake__additional_security_email_configured(framework, check_id) %}
+SELECT
+  id,
+  '{{framework}}' As framework,
+  '{{check_id}}' As check_id,
+  'Ensure "Additional email addresses" is Configured with a Security Contact Email (Automated)' as title,
+  subscription_id,
+  case
+    when properties:email is not null
+    then 'pass' else 'fail'
+  end
+FROM azure_security_contacts asc
+where name = "default"
+{% endmacro %}
+
+{% macro bigquery__additional_security_email_configured(framework, check_id) %}
+SELECT
+  id,
+  '{{framework}}' As framework,
+  '{{check_id}}' As check_id,
+  'Ensure "Additional email addresses" is Configured with a Security Contact Email (Automated)' as title,
+  subscription_id,
+  case
+    when (JSON_VALUE(properties.email) is not null or JSON_VALUE(properties.email) != "")
+    then 'pass' else 'fail'
+  end
+FROM {{ full_table_name("azure_security_contacts") }} asc
+where name = "default"
 {% endmacro %}
