9689473b0381544e6da00c58f8475ef1f548a864
john-s58 <jonathansarig58@gmail.com>
2024-02-09 01:53:20 +0200

    added new models and macros

---
+++ transformations/aws/cost/models/aws_cost__by_product.sql
@@ -0,0 +1,5 @@
+SELECT line_item_product_code, SUM(line_item_unblended_cost) as cost
+FROM {{ var('cost_usage_table') }}
+GROUP BY line_item_product_code
+HAVING SUM(line_item_unblended_cost) > 0
+ORDER BY SUM(line_item_unblended_cost) DESC;
--- transformations/aws/cost/models/aws_cost__compute_optimizer_results.sql
+++ transformations/aws/cost/models/aws_cost__compute_optimizer_results.sql
@@ -1,17 +1,26 @@
-SELECT *
-FROM (  
-	SELECT account_id,  instance_arn as arn, recommendation_options as roptions
-  	FROM aws_computeoptimizer_ec2_instance_recommendations
-  	UNION
-  	SELECT account_id,  function_arn as arn, memory_size_recommendation_options as roptions 
-  	FROM aws_computeoptimizer_lambda_function_recommendations
-  	UNION
-  	SELECT account_id, volume_arn as arn, volume_recommendation_options as roptions 
-  	FROM aws_computeoptimizer_ebs_volume_recommendations 
-  	UNION
-	SELECT account_id, service_arn as arn, service_recommendation_options as roptions
-	FROM aws_computeoptimizer_ecs_service_recommendations
-  	UNION
-	SELECT account_id, auto_scaling_group_arn as arn,  recommendation_options as roptions
-	from aws_computeoptimizer_autoscaling_group_recommendations
-) as compute_opt
+with computeoptimizer as (
+SELECT account_id,  instance_arn as arn, recommendation_options as roptions
+FROM aws_computeoptimizer_ec2_instance_recommendations
+UNION
+SELECT account_id,  function_arn as arn, memory_size_recommendation_options as roptions 
+FROM aws_computeoptimizer_lambda_function_recommendations
+UNION
+SELECT account_id, volume_arn as arn, volume_recommendation_options as roptions 
+FROM aws_computeoptimizer_ebs_volume_recommendations 
+UNION
+SELECT account_id, service_arn as arn, service_recommendation_options as roptions
+FROM aws_computeoptimizer_ecs_service_recommendations
+UNION
+SELECT account_id, auto_scaling_group_arn as arn,  recommendation_options as roptions
+from aws_computeoptimizer_autoscaling_group_recommendations),
+total_cost as (
+SELECT line_item_resource_id as arn, SUM(line_item_unblended_cost) as sum_cost
+FROM john_cost_00001_snappy
+GROUP BY line_item_resource_id
+)
+SELECT computeoptimizer.*, rc.cost
+FROM computeoptimizer
+LEFT JOIN {{ ref('aws_cost__by_resources') }} rc
+	ON computeoptimizer.arn = rc.line_item_resource_id
+
+
