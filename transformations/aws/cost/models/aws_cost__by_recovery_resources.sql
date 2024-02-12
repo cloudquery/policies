4b34482278fb13627cf1bb07acd55af231932fc2
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-02-12 13:21:08 +0200

    feat: Added model aws_cost__by_recovery_resources

---
+++ transformations/aws/cost/models/aws_cost__by_recovery_resources.sql
@@ -0,0 +1,37 @@
+with
+    aggregated as (
+		({{ recovery_rds_db_snapshots() }})
+    {{ union() }}
+		({{ recovery_rds_cluster_snapshots() }})
+    {{ union() }}
+		({{ recovery_docdb_cluster_snapshots() }})
+    {{ union() }}
+		({{ recovery_elasticache_snapshots() }})
+		{{ union() }}
+		({{ recovery_neptune_cluster_snapshots() }})
+    {{ union() }}
+		({{ recovery_fsx_snapshots() }})
+    {{ union() }}
+		({{ recovery_ec2_ebs_snapshots() }})
+    {{ union() }}
+		({{ recovery_redshift_snapshots() }})
+    {{ union() }}
+		({{ recovery_lightsail_instance_snapshots() }})
+    {{ union() }}
+		({{ recovery_lightsail_disk_snapshots() }})
+    {{ union() }}
+		({{ recovery_lightsail_database_snapshots() }})
+    {{ union() }}
+		({{ recovery_cloudhsmv2_backups() }})
+    {{ union() }}
+		({{ recovery_dynamodb_table_continuous_backups() }})
+    {{ union() }}
+		({{ recovery_dynamodb_backups() }})
+    {{ union() }}
+		({{ recovery_fsx_backups() }})
+
+    )
+select 
+        {{ gen_timestamp() }},
+        aggregated.*
+from aggregated
