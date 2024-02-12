dfd662340b118704af55a0019d188c95b9827355
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-02-12 09:31:53 -0500

    fixing branch for PR

---
+++ transformations/aws/cost/models/aws_cost__by_account.sql
@@ -0,0 +1,6 @@
+select
+bill_payer_account_id as account_id,
+sum(line_item_unblended_cost) as cost
+from {{ var('cost_usage_table') }}
+group by bill_payer_account_id
+having sum(line_item_unblended_cost) > 0
