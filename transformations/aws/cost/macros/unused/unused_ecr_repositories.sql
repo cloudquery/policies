c2b7dfe60b7a1bc28cef505570f624414e8211d8
john-s58 <jonathansarig58@gmail.com>
2024-02-13 14:26:53 +0200

    reverted macro to original form

--- transformations/aws/cost/macros/unused/unused_ecr_repositories.sql
+++ transformations/aws/cost/macros/unused/unused_ecr_repositories.sql
@@ -5,18 +5,20 @@
 {% macro default__unused_ecr_repositories(framework, check_id) %}{% endmacro %}
 
 {% macro postgres__unused_ecr_repositories(framework, check_id) %}
+with image as (select distinct account_id, repository_name from aws_ecr_repository_images),
+unused_repo as (
+select 
+       repository.account_id,
+       repository.arn          as resource_id
+from aws_ecr_repositories repository
+         left join image on image.account_id = repository.account_id and image.repository_name = repository.repository_name
+where image.repository_name is null)
 SELECT 
     ur.account_id,
     ur.resource_id,
     rbc.cost,
        'ecr_repositories' as resource_type
-FROM (
-select 
-       repository.account_id,
-       repository.arn          as resource_id
-from aws_ecr_repositories repository
-         left join (select distinct ri.account_id, ri.repository_name from aws_ecr_repository_images ri) image on image.account_id = repository.account_id and image.repository_name = repository.repository_name
-where image.repository_name is null) ur
+FROM unused_repo ur
 JOIN {{ ref('aws_cost__by_resources') }} rbc ON ur.resource_id = rbc.line_item_resource_id
 {% endmacro %}
 
