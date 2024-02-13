f23843c6af65bf4e8a44fbc4596cf3cf625c790e
john-s58 <jonathansarig58@gmail.com>
2024-02-13 12:38:01 +0200

    swithced all CTEs to subqueries to fix union issue in the unused model, updated dbt_project to include macro path

--- transformations/aws/cost/macros/unused/unused_backup_vaults.sql
+++ transformations/aws/cost/macros/unused/unused_backup_vaults.sql
@@ -4,24 +4,22 @@
 
 {% macro default__unused_backup_vaults(framework, check_id) %}{% endmacro %}
 
-{% macro postgres__unused_backup_vaults(framework, check_id) %}
-with point as (
-    select distinct vault_arn 
-    from aws_backup_vault_recovery_points
-    ),
-    unused_vaults as (
-select
-       vault.account_id,
-       vault.arn                        as resource_id
-from aws_backup_vaults vault
-         left join point on point.vault_arn = vault.arn
-where point.vault_arn is null)
+{% macro postgres__unused_backup_vaults(framework, check_id) %} 
 SELECT 
     uv.account_id,
     uv.resource_id,
     rbc.cost,
     'backup_vaults' as resource_type
-FROM unused_vaults uv
+FROM (
+select
+       vault.account_id,
+       vault.arn                        as resource_id
+from aws_backup_vaults vault
+         left join (
+    select distinct rp.vault_arn 
+    from aws_backup_vault_recovery_points as rp
+    ) as point on point.vault_arn = vault.arn
+where point.vault_arn is null) as uv
 JOIN {{ ref('aws_cost__by_resources') }} rbc ON uv.resource_id = rbc.line_item_resource_id
 {% endmacro %}
 
--- transformations/aws/cost/macros/unused/unused_ec2_transit_gateways.sql
+++ transformations/aws/cost/macros/unused/unused_ec2_transit_gateways.sql
@@ -5,20 +5,18 @@
 {% macro default__unused_ec2_transit_gateways(framework, check_id) %}{% endmacro %}
 
 {% macro postgres__unused_ec2_transit_gateways(framework, check_id) %}
-with attachment as (select distinct transit_gateway_arn from aws_ec2_transit_gateway_attachments),
-unused_transit_gateways as (
-select 
-       gateway.account_id,
-       gateway.arn              as resource_id
-from aws_ec2_transit_gateways gateway
-         left join attachment on attachment.transit_gateway_arn = gateway.arn
-where attachment.transit_gateway_arn is null)
 SELECT 
     ug.account_id,
     ug.resource_id,
     rbc.cost,
        'ec2_transit_gateways' as resource_type
-FROM unused_transit_gateways ug
+FROM (
+select 
+       gateway.account_id,
+       gateway.arn              as resource_id
+from aws_ec2_transit_gateways gateway
+         left join (select distinct ga.transit_gateway_arn from aws_ec2_transit_gateway_attachments ga) attachment on attachment.transit_gateway_arn = gateway.arn
+where attachment.transit_gateway_arn is null) ug
 JOIN {{ ref('aws_cost__by_resources') }} rbc ON ug.resource_id = rbc.line_item_resource_id
 {% endmacro %}
 
