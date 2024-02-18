4ebe70bdd6413d5461ebc8ea4867959e6b8d73cb
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-02-18 13:51:09 +0200

    feat: Added model aws_cost__by_recovery_resources

---
+++ transformations/aws/macros/recovery/recovery_fsx_backups.sql
@@ -0,0 +1,26 @@
+{% macro recovery_fsx_backups(framework, check_id) %}
+  {{ return(adapter.dispatch('recovery_fsx_backups')(framework, check_id)) }}
+{% endmacro %}
+
+{% macro default__recovery_fsx_backups(framework, check_id) %}{% endmacro %}
+
+{% macro postgres__recovery_fsx_backups(framework, check_id) %}
+with fsx_backups_cost as (
+SELECT line_item_resource_id as resource_id, SUM(line_item_unblended_cost) as unblended_cost
+FROM {{ var('cost_usage_table') }}
+WHERE product_product_name = 'Amazon FSx'
+GROUP BY line_item_resource_id
+) 
+SELECT
+	account_id,
+    resource_arn as resource_id,
+    unblended_cost as cost,
+    'fsx_backups' as resource_type
+from aws_fsx_backups as afb
+inner join fsx_backups_cost as fbc
+on afb.resource_arn = fbc.resource_id
+{% endmacro %}
+
+{% macro snowflake__recovery_fsx_backups(framework, check_id) %}
+
+{% endmacro %}
---
+++ transformations/aws/macros/recovery/recovery_rds_db_snapshots.sql
@@ -0,0 +1,26 @@
+{% macro recovery_rds_db_snapshots() %}
+  {{ return(adapter.dispatch('recovery_rds_db_snapshots')()) }}
+{% endmacro %}
+
+{% macro default__recovery_rds_db_snapshots() %}{% endmacro %}
+
+{% macro postgres__recovery_rds_db_snapshots() %}
+with rds_db_snapshots_cost as (
+SELECT line_item_resource_id as resource_id, SUM(line_item_unblended_cost) as unblended_cost
+FROM {{ var('cost_usage_table') }}
+WHERE product_product_name = 'Amazon RDS'
+GROUP BY line_item_resource_id
+) 
+SELECT
+    account_id,
+    arn as resource_id,
+    unblended_cost as cost,
+    'rds_db_snapshots' as resource_type
+from aws_rds_db_snapshots as ardbs
+inner join rds_db_snapshots_cost as rdbsc
+on ardbs.arn = rdbsc.resource_id
+{% endmacro %}
+
+{% macro snowflake__recovery_rds_db_snapshots() %}
+
+{% endmacro %}
