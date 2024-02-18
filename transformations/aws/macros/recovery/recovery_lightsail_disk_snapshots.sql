1e44a5c7eaef7f82067a5c4a2d1b4198a97de1ce
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-02-19 01:05:16 +0200

    feat: Added model aws_cost__by_recovery_resources (#597)

---
+++ transformations/aws/macros/recovery/recovery_lightsail_disk_snapshots.sql
@@ -0,0 +1,26 @@
+{% macro recovery_lightsail_disk_snapshots(framework, check_id) %}
+  {{ return(adapter.dispatch('recovery_lightsail_disk_snapshots')(framework, check_id)) }}
+{% endmacro %}
+
+{% macro default__recovery_lightsail_disk_snapshots(framework, check_id) %}{% endmacro %}
+
+{% macro postgres__recovery_lightsail_disk_snapshots(framework, check_id) %}
+with disk_snapshots_cost as (
+SELECT line_item_resource_id as resource_id, SUM(line_item_unblended_cost) as unblended_cost
+FROM {{ var('cost_usage_table') }}
+WHERE product_product_name = 'Amazon Lightsail'
+GROUP BY line_item_resource_id
+)
+select 
+		account_id,
+    arn as resource_id,
+    unblended_cost as cost,
+    'lightsail_disk_snapshots' as resource_type
+from aws_lightsail_disk_snapshots as lds
+inner join disk_snapshots_cost as dsc
+on lds.arn = dsc.resource_id
+{% endmacro %}
+
+{% macro snowflake__recovery_lightsail_disk_snapshots(framework, check_id) %}
+
+{% endmacro %}
