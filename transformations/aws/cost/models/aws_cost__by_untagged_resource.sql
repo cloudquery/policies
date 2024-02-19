dd24c75a985e34f7029e2fcebe195ead8631fdf1
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-02-19 14:42:36 -0500

    updated some column names for clarity

--- transformations/aws/cost/models/aws_cost__by_untagged_resource.sql
+++ transformations/aws/cost/models/aws_cost__by_untagged_resource.sql
@@ -1,11 +1,11 @@
 select 
-bill_payer_account_id,
-line_item_resource_id,
-product_servicecode,
+bill_payer_account_id as account_id,
+line_item_resource_id as resource_id,
+product_servicecode as service,
 extract(month from bill_billing_period_start_date) as month,
 sum(line_item_unblended_cost) as sum_line_item_unblended_cost
 from
 {{ var('cost_usage_table') }}
 where resource_tags = '[]' or resource_tags is null
-group by bill_payer_account_id, line_item_resource_id, product_servicecode, month
+group by account_id, resource_id, service, month
 having sum(line_item_unblended_cost) > 0;
