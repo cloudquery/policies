e2853a7574f48c6bf1ac083e4474fb30118c787e
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-02-21 00:39:25 +0200

    feat: Added model aws_cost__trusted_advisior_by_arn (#605)

---
+++ transformations/aws/cost/models/aws_cost__trusted_advisior_by_arn.sql
@@ -0,0 +1,14 @@
+SELECT
+    checks.account_id,
+    arn.value::text AS arn,
+    checks.category,
+    checks.id,
+    checks.name,
+    checks.description
+FROM
+    aws_support_trusted_advisor_checks AS checks
+    LEFT JOIN aws_support_trusted_advisor_check_results AS results ON checks.id = results.check_id,
+	jsonb_array_elements(results.flagged_resources->'resourceId') AS arn
+WHERE
+    category = 'cost_optimizing'
+    AND checks.language_code = 'en'
