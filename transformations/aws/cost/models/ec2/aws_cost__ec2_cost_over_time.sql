7f6f7d8ea4f175c20925653812fe1e6990937418
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-03-13 18:20:47 +0200

    feat: Added models for EC2 dashboard (#646)
    * feat: Added models for ec2 dashboard
    
    * feat: Added models for ec2 dashboard - CUR2

---
+++ transformations/aws/cost/models/ec2/aws_cost__ec2_cost_over_time.sql
@@ -0,0 +1,11 @@
+SELECT
+  DATE(line_item_usage_end_date) AS usage_date,
+  SUM(line_item_unblended_cost) AS total_cost
+FROM {{ var('cost_usage_table') }}
+WHERE
+  line_item_product_code = 'AmazonEC2'
+  AND
+  line_item_resource_id like 'i-%'
+GROUP BY DATE(line_item_usage_end_date)
+HAVING SUM(line_item_unblended_cost) > 0
+ORDER BY usage_date ASC
