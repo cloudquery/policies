265bd46bb0ffdff6dd640430f8cf7fa1e166cd15
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-03-19 17:49:19 -0400

    reworked macro for aws cis 3.0 and back

--- transformations/aws/macros/config/config_enabled_all_regions.sql
+++ transformations/aws/macros/config/config_enabled_all_regions.sql
@@ -3,57 +3,101 @@
 {% endmacro %}
 
 {% macro snowflake__config_enabled_all_regions(framework, check_id) %}
-select
-    '{{framework}}' As framework,
-    '{{check_id}}' As check_id,
-    'AWS Config should be enabled' as title,
-    account_id,
-    arn as resource_id,
-    CASE 
-        WHEN ((recording_group:IncludeGlobalResourceTypes::BOOLEAN != TRUE) OR (recording_group:AllSupported::BOOLEAN != TRUE) OR (status_recording != TRUE OR status_last_status != 'SUCCESS'))
-    THEN 'fail'
-    ELSE 'pass'
-    END AS status
-FROM
+with global_recorders as (
+  select
+    count(*) as global_config_recorders
+  from
     aws_config_configuration_recorders
+  where
+    recording_group:IncludeGlobalResourceTypes::BOOLEAN = TRUE
+    and recording_group:AllSupported::BOOLEAN = TRUE
+    and status_recording = TRUE
+    and status_last_status = 'SUCCESS'
+)
+select
+	'{{framework}}' As framework,
+	'{{check_id}}' As check_id,
+    'AWS Config should be enabled' as title,
+    r.account_id,
+    r.arn as resource_id,
+  case
+    when g.global_config_recorders >= 1
+    and status_recording = TRUE
+    and status_last_status = 'SUCCESS' then 'pass'
+    else 'fail'
+  end as status
+from
+  global_recorders g,
+  aws_regions a
+  inner join aws_config_configuration_recorders as r on r.account_id = a.account_id
+  and r.region = a.region_name
+  where a.opt_in_status != 'not-opted-in'
 {% endmacro %}
 
 {% macro postgres__config_enabled_all_regions(framework, check_id) %}
-select
-    '{{framework}}' as framework,
-    '{{check_id}}' as check_id,
-    'AWS Config should be enabled' as title,
-    account_id,
-    arn as resource_id,
-    case when
-      (recording_group->>'IncludeGlobalResourceTypes')::boolean IS NOT TRUE
-      OR (recording_group->>'AllSupported')::boolean IS NOT TRUE
-      OR status_recording IS NOT TRUE
-      OR status_last_status IS DISTINCT FROM 'SUCCESS'
-    then 'fail'
-    else 'pass'
-    end as status
-FROM
+with global_recorders as (
+  select
+    count(*) as global_config_recorders
+  from
     aws_config_configuration_recorders
+  where
+    recording_group -> 'IncludeGlobalResourceTypes' = 'true'
+    and recording_group -> 'AllSupported' = 'true'
+    and status_recording is true
+    and status_last_status = 'SUCCESS'
+)
+select
+	'{{framework}}' As framework,
+	'{{check_id}}' As check_id,
+    'AWS Config should be enabled' as title,
+    r.account_id,
+    r.arn as resource_id,
+  case
+    when g.global_config_recorders >= 1
+    and status_recording is true
+    and status_last_status = 'SUCCESS' then 'pass'
+    else 'fail'
+  end as status
+from
+  global_recorders g,
+  aws_regions a
+  inner join aws_config_configuration_recorders r on r.account_id = a.account_id
+  and r.region = a.region_name
+  where a.opt_in_status != 'not-opted-in'
 {% endmacro %}
 
 {% macro default__config_enabled_all_regions(framework, check_id) %}{% endmacro %}
                     
 {% macro bigquery__config_enabled_all_regions(framework, check_id) %}
+with global_recorders as (
+  select
+    count(*) as global_config_recorders
+  from
+    kevinr_test.aws_config_configuration_recorders
+  where
+    CAST( JSON_VALUE(recording_group.IncludeGlobalResourceTypes) AS BOOL) IS TRUE
+    and CAST( JSON_VALUE(recording_group.AllSupported) AS BOOL) IS TRUE
+    and status_recording is true
+    and status_last_status = 'SUCCESS'
+)
 select
+    distinct
     '{{framework}}' as framework,
     '{{check_id}}' as check_id,
     'AWS Config should be enabled' as title,
-    account_id,
-    arn as resource_id,
-    case when
-      CAST( JSON_VALUE(recording_group.IncludeGlobalResourceTypes) AS BOOL) IS NOT TRUE
-      OR CAST( JSON_VALUE(recording_group.AllSupported) AS BOOL) IS NOT TRUE
-      OR status_recording IS NOT TRUE
-      OR status_last_status IS DISTINCT FROM 'SUCCESS'
-    then 'fail'
-    else 'pass'
-    end as status
-FROM
-    {{ full_table_name("aws_config_configuration_recorders") }}
+    r.account_id,
+    r.arn as resource_id,
+    case 
+    when a.opt_in_status = 'not-opted-in' then ''
+    when g.global_config_recorders >= 1
+    and status_recording is true
+    and status_last_status = 'SUCCESS' then 'pass'
+    else 'fail'
+  end as status
+from
+  global_recorders g,
+  {{ full_table_name("aws_regions") }} a
+  inner join {{ full_table_name("aws_config_configuration_recorders") }} r on r.account_id = a.account_id
+  and r.region = a.region_name
+  where a.opt_in_status != 'not-opted-in'
 {% endmacro %}
