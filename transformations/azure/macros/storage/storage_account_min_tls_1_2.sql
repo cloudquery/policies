ed436e3eb401608cae73e82e5dd73115dbcb2160
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-04-07 17:03:39 +0300

    feat: Added CIS Azure v2.1.0, snowflake and bigquery destination (#733)
    * feat: Added CIS Azure v2.1.0 - snowflake destination
    
    * Added CIS Azure v2.1.0 - bigquery destination
    
    * Some corrections

--- transformations/azure/macros/storage/storage_account_min_tls_1_2.sql
+++ transformations/azure/macros/storage/storage_account_min_tls_1_2.sql
@@ -20,7 +20,31 @@ FROM azure_storage_accounts
 {% endmacro %}
 
 {% macro snowflake__storage_account_min_tls_1_2(framework, check_id) %}
+SELECT
+    id                                                                     AS resource_id,
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'Ensure the "Minimum TLS version" for storage accounts is set to "Version 1.2"' AS title,
+    subscription_id                                                        AS subscription_id,
+    CASE
+        WHEN properties:minimumTlsVersion != 'TLS1_2'
+        THEN 'fail'
+        ELSE 'pass'
+    END                                                                         AS status
+FROM azure_storage_accounts
 {% endmacro %}
 
 {% macro bigquery__storage_account_min_tls_1_2(framework, check_id) %}
+SELECT
+    id                                                                     AS resource_id,
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'Ensure the "Minimum TLS version" for storage accounts is set to "Version 1.2"' AS title,
+    subscription_id                                                        AS subscription_id,
+    CASE
+        WHEN JSON_VALUE(properties.minimumTlsVersion) != 'TLS1_2'
+        THEN 'fail'
+        ELSE 'pass'
+    END                                                                         AS status
+FROM {{ full_table_name("azure_storage_accounts") }}
 {% endmacro %}
