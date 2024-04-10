878c907bc9960a76a0ca9bcf4e9868da469b8c4d
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-04-10 11:45:47 +0300

    feat: Added macro check_tables

---
+++ transformations/macros/check_tables.sql
@@ -0,0 +1,20 @@
+{% macro check_tables_exist(variable_name) %}
+    {% set tables = var(variable_name, []) %}
+    {% for table in tables %}
+        {% set query %}
+            SELECT COUNT(*)
+            FROM information_schema.tables
+            WHERE table_schema = '{{ target.schema }}'
+            AND table_name = '{{ table }}'
+        {% endset %}
+        {% set result = run_query(query) %}
+        {% if result %}
+            {% set table_exists = result.rows[0][0] | int > 0 %}
+            {% if not table_exists %}
+                {{ log("Table '" ~ table ~ "' does not exist in schema '" ~ target.schema ~ "'.", "error") }}
+            {% endif %}
+        {% else %}
+            {{ log("Could not check existence of table '" ~ table ~ "'.", "warn") }}
+        {% endif %}
+    {% endfor %}
+{% endmacro %}
