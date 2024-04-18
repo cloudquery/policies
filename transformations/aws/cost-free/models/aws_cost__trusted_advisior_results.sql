36af7663e8cb9a3a3f2fa024a7617068fc4c50dd
k-rheinheimer <114611405+k-rheinheimer@users.noreply.github.com>
2024-04-18 09:13:02 -0400

    fix: Deleting AWS cost free policy (#779)

--- transformations/aws/cost-free/models/aws_cost__trusted_advisior_results.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM (
-	SELECT checks.account_id, category, description, id, name, flagged_resources, resources_summary, status, timestamp
-	FROM aws_support_trusted_advisor_checks as checks
-	LEFT JOIN aws_support_trusted_advisor_check_results as results
-	ON checks.id = results.check_id
-	WHERE category='cost_optimizing' and checks.language_code='en' 
-) as trust_adv
