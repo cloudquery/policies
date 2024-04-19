6fd367aaf6291488750f6fefabea50a590cae062
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-04-19 08:53:37 -0400

    athena progress

--- transformations/aws/macros/apigateway/api_gw_execution_logging_enabled.sql
+++ transformations/aws/macros/apigateway/api_gw_execution_logging_enabled.sql
@@ -38,7 +38,7 @@ left join
     aws_apigatewayv2_apis a on s.api_arn = a.arn
 )
 {% endmacro %}
-)
+
 
 {% macro postgres__api_gw_execution_logging_enabled(framework, check_id) %}
 (select distinct
@@ -112,4 +112,41 @@ from
 left join
     {{ full_table_name("aws_apigatewayv2_apis") }} a on s.api_arn = a.arn
 )
-{% endmacro %}       
+{% endmacro %}   
+
+{% macro athena__api_gw_execution_logging_enabled(framework, check_id) %}
+
+(select distinct
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'API Gateway REST and WebSocket API logging should be enabled' as title,
+    r.account_id,
+    'arn:' || 'aws' || ':apigateway:' || r.region || ':/restapis/' || r.id as resource_id,
+    case
+        when s.logging_level not in ('"ERROR"', '"INFO"') then 'fail'
+        else 'pass'
+    end as status
+from
+    {{ ref('aws_compliance__api_gateway_method_settings') }} s
+left join
+    aws_apigateway_rest_apis r on s.rest_api_arn = r.arn
+)
+
+union
+
+(select distinct
+     '{{framework}}' As framework,
+     '{{check_id}}' As check_id,
+     'API Gateway REST and WebSocket API logging should be enabled' as title,
+     a.account_id,
+     'arn:' || 'aws' || ':apigateway:' || a.region || ':/apis/' || a.id as resource_id,
+     case
+         WHEN s.default_route_settings:LoggingLevel IN (NULL, 'OFF') THEN 'fail'
+         else 'pass'
+         end as status
+from
+    aws_apigatewayv2_api_stages s
+left join
+    aws_apigatewayv2_apis a on s.api_arn = a.arn
+)
+{% endmacro %}
