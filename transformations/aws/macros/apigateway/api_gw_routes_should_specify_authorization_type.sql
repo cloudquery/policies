1f37c4d52d5ec4867e849dbdd83f0100e6e07b9e
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-04-19 12:50:25 -0400

    athena macros progress

--- transformations/aws/macros/apigateway/api_gw_routes_should_specify_authorization_type.sql
+++ transformations/aws/macros/apigateway/api_gw_routes_should_specify_authorization_type.sql
@@ -48,3 +48,18 @@ select
 FROM 
     {{ full_table_name("aws_apigatewayv2_api_routes") }}
 {% endmacro %}
+
+{% macro athena__api_gw_routes_should_specify_authorization_type(framework, check_id) %}
+select
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'API Gateway routes should specify an authorization type' as title,
+    account_id,
+    arn as resource_id,
+    CASE
+        WHEN authorization_type IS NULL OR authorization_type = '' OR authorization_type = 'NONE' THEN 'fail'
+        ELSE 'pass'
+    END AS status
+FROM 
+    aws_apigatewayv2_api_routes
+{% endmacro %}
