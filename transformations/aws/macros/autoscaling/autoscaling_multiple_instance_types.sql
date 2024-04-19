a62d68b60deb3202dd3b921219312780c3a2b2d0
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-04-19 14:19:39 -0400

    more progress

--- transformations/aws/macros/autoscaling/autoscaling_multiple_instance_types.sql
+++ transformations/aws/macros/autoscaling/autoscaling_multiple_instance_types.sql
@@ -72,3 +72,26 @@ JOIN (
   GROUP BY arn
 ) AS ditc ON aag.arn = ditc.arn
 {% endmacro %}
+
+{% macro athena__autoscaling_multiple_instance_types(framework, check_id) %}
+select
+  '{{framework}}' As framework,
+  '{{check_id}}' As check_id,
+  'Auto Scaling groups should use multiple instance types in multiple Availability Zones' AS "title",
+  aag.account_id,
+  ditc.arn AS resource_id,
+  ditc.status
+FROM aws_autoscaling_groups as aag
+JOIN (
+  SELECT
+    arn,
+    CASE
+      WHEN COUNT(DISTINCT json_extract(instance, '$.value.InstanceType')) > 1 THEN 'pass'
+      ELSE 'fail'
+    END AS status
+  FROM
+    aws_autoscaling_groups AS aag,
+    LATERAL FLATTEN(input => aag.INSTANCES) instance
+  GROUP BY arn
+) AS ditc ON aag.arn = ditc.arn
+{% endmacro %}
