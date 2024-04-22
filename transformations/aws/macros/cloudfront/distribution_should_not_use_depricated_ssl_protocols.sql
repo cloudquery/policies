1176693217e175305dad387356be823c56f84b52
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-04-22 09:41:42 -0400

    progress

--- transformations/aws/macros/cloudfront/distribution_should_not_use_depricated_ssl_protocols.sql
+++ transformations/aws/macros/cloudfront/distribution_should_not_use_depricated_ssl_protocols.sql
@@ -86,3 +86,32 @@ FROM
     LEFT JOIN origins_with_sslv3 o
     ON d.arn = o.arn
 {% endmacro %}
+
+--todo
+{% macro athena__distribution_should_not_use_depricated_ssl_protocols(framework, check_id) %}
+WITH origins_with_sslv3 AS (
+SELECT DISTINCT
+    arn,
+    o.value:Id AS origin_id
+FROM
+    aws_cloudfront_distributions
+, LATERAL FLATTEN(input => COALESCE(distribution_config:Origins:Items, ARRAY_CONSTRUCT())) AS o
+WHERE
+    ARRAY_CONTAINS('SSLv3'::variant, o.value:CustomOriginConfig:OriginSslProtocols:Items::ARRAY)
+)
+
+SELECT
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'CloudFront distributions should not use deprecated SSL protocols between edge locations and custom origins' as title,
+    d.arn as resource_id,
+    d.account_id,
+    CASE
+        WHEN o.arn is null THEN 'pass'
+        ELSE 'fail'
+    END as status
+FROM
+    aws_cloudfront_distributions d
+    LEFT JOIN origins_with_sslv3 o
+    ON d.arn = o.arn
+{% endmacro %}
--- transformations/aws/macros/cloudfront/viewer_policy_https.sql
+++ transformations/aws/macros/cloudfront/viewer_policy_https.sql
@@ -73,3 +73,29 @@ select
     'fail' as status
 from cachebeviors
 {% endmacro %}
+
+{% macro athena__viewer_policy_https(framework, check_id) %} --todo handle lateral flattens for athena
+wITH cachebeviors AS (
+    -- Handle all non-defaults as well as when there is only a default route
+    SELECT DISTINCT arn, account_id 
+    FROM (
+        SELECT arn, account_id, d.value AS CacheBehavior 
+        FROM aws_cloudfront_distributions, 
+        LATERAL FLATTEN(input => distribution_config:CacheBehaviors:Items) AS d 
+        WHERE distribution_config:CacheBehaviors:Items IS NOT NULL
+        UNION 
+        -- Handle default Cachebehaviors
+        SELECT arn, account_id, distribution_config:DefaultCacheBehavior AS CacheBehavior 
+        FROM aws_cloudfront_distributions
+    ) AS cachebeviors 
+    WHERE CacheBehavior:ViewerProtocolPolicy::STRING = 'allow-all'
+)
+select
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'CloudFront distributions should require encryption in transit' as title,
+    account_id,
+    arn as resource_id,
+    'fail' as status
+from cachebeviors
+{% endmacro %}
