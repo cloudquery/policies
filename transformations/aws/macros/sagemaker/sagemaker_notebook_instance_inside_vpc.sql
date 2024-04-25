f3c3a0b92c2937de0347f7ffcde3c8e15aba935e
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-04-25 15:58:46 -0400

    finished sagemaker and secretsmanager

--- transformations/aws/macros/sagemaker/sagemaker_notebook_instance_inside_vpc.sql
+++ transformations/aws/macros/sagemaker/sagemaker_notebook_instance_inside_vpc.sql
@@ -42,3 +42,16 @@ select
         then 'pass' else 'fail' end as status
 from {{ full_table_name("aws_sagemaker_notebook_instances") }}
 {% endmacro %}
+
+{% macro athena__sagemaker_notebook_instance_inside_vpc(framework, check_id) %}
+select
+    '{{framework}}' As framework,
+    '{{check_id}}' As check_id,
+    'SageMaker notebook instances should be launched in a custom VPC' as title,
+    account_id,
+    arn as resource_id,
+    case when
+        subnet_id is not null
+        then 'pass' else 'fail' end as status
+from aws_sagemaker_notebook_instances
+{% endmacro %}
