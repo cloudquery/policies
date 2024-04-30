022c7f6ffb6ab45a6a4535857105ad3c1c37fc6f
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-04-30 15:18:27 -0400

    adding athena macros for aws imds v2

--- transformations/aws/macros/ec2/ec2_not_imdsv2_instances.sql
+++ transformations/aws/macros/ec2/ec2_not_imdsv2_instances.sql
@@ -51,3 +51,19 @@ select
 from aws_ec2_instances
 where metadata_options:httpEndpoint = 'enabled'
 {% endmacro %}
+
+{% macro athena__ec2_not_imdsv2_instances(framework, check_id) %}
+select
+  '{{framework}}' as framework,
+  '{{check_id}}' as check_id,
+  'EC2 instances should use IMDSv2' as title,
+  account_id,
+  instance_id as resource_id,
+  case when
+    json_extract_scalar(metadata_options, '$.HttpTokens') is distinct from 'required'
+    then 'fail'
+    else 'pass'
+  end as status
+from aws_ec2_instances
+where json_extract_scalar(metadata_options, '$.httpEndpoint') = 'enabled'
+{% endmacro %}
