2084bba312eaa966f4aed389d43d5473f6dd633a
john-s58 <jonathansarig58@gmail.com>
2024-05-02 15:21:57 +0300

    removed duplicate macro

--- transformations/aws/macros/iam/policies_attached_to_groups_roles.sql
+++ transformations/aws/macros/iam/policies_attached_to_groups_roles.sql
@@ -57,20 +57,6 @@ left join {{ full_table_name("aws_iam_user_policies") }} on aws_iam_users.arn = 
 {% endmacro %}
 
 {% macro athena__policies_attached_to_groups_roles(framework, check_id) %}
-select distinct
-    '{{framework}}' As framework,
-    '{{check_id}}' As check_id,
-    'IAM users should not have IAM policies attached' as title,
-    aws_iam_users.account_id,
-    arn AS resource_id,
-    case when
-        aws_iam_user_attached_policies.user_arn is not null
-    then 'fail' else 'pass' end as status
-from aws_iam_users
-left join aws_iam_user_attached_policies on aws_iam_users.arn = aws_iam_user_attached_policies.user_arn
-{% endmacro %}
-
-{% macro athena__policies_attached_to_groups_roles(framework, check_id) %}
 SELECT DISTINCT
     '{{framework}}' AS framework,
     '{{check_id}}' AS check_id,
