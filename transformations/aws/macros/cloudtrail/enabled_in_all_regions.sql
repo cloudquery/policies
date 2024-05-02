cbe4e017e1d66c63494753f27427e08d885eb48e
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-05-01 20:35:16 -0400

    even more macros

--- transformations/aws/macros/cloudtrail/enabled_in_all_regions.sql
+++ transformations/aws/macros/cloudtrail/enabled_in_all_regions.sql
@@ -2,7 +2,9 @@
   {{ return(adapter.dispatch('cloudtrail_enabled_all_regions')(framework, check_id)) }}
 {% endmacro %}
 
-{% macro default__cloudtrail_enabled_all_regions(framework, check_id) %}
+{% macro default__cloudtrail_enabled_all_regions(framework, check_id) %}{% endmacro %}
+
+{% macro postgres__cloudtrail_enabled_all_regions(framework, check_id) %}
 select
     '{{framework}}' as framework,
     '{{check_id}}' as check_id,
@@ -87,6 +89,44 @@ select
                    from aes,
                   LATERAL FLATTEN (value:FieldSelectors) as aes_fs
                     where aes_fs.value:Field = 'readOnly'
+                   )
+            then 'fail'
+        else 'pass'
+    end as status
+from aws_cloudtrail_trails
+inner join
+    aws_cloudtrail_trail_event_selectors on
+        aws_cloudtrail_trails.arn = aws_cloudtrail_trail_event_selectors.trail_arn
+        and aws_cloudtrail_trails.region = aws_cloudtrail_trail_event_selectors.region
+        and aws_cloudtrail_trails.account_id = aws_cloudtrail_trail_event_selectors.account_id
+{% endmacro %}
+
+{% macro athena__cloudtrail_enabled_all_regions(framework, check_id) %}
+with aes as
+(
+  select *
+  from aws_cloudtrail_trail_event_selectors,
+  unnest(cast(json_parse(advanced_event_selectors) as array(json))) as t(aes)
+)
+select distinct
+    '{{framework}}' as framework,
+    '{{check_id}}' as check_id,
+    'Ensure CloudTrail is enabled in all regions' as title,
+    aws_cloudtrail_trails.account_id,
+    arn as resource_id,
+    case
+        when aws_cloudtrail_trails.is_multi_region_trail = FALSE then 'fail'
+        when exists(select *
+                    from aws_cloudtrail_trail_event_selectors,
+                    unnest(cast(json_parse(event_selectors) as array(json))) as t(es)
+                    where json_extract_scalar(es, '$.ReadWriteType') != 'All' or cast(json_extract_scalar(es, '$.IncludeManagementEvents') as boolean) = FALSE
+                    )
+            then 'fail'
+        when exists(
+                    select *
+                   from aes,
+                   unnest(cast(json_extract(aes, '$.FieldSelectors') as array(json))) as t(aes_fs)
+                    where json_extract_scalar(aes_fs, '$.Field') = 'readOnly'
                    )
             then 'fail'
         else 'pass'
