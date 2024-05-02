d2e7ac2796dccf442d097c74a010aa67f592bc24
Kevin Rheinheimer <rheinheimerk@gmail.com>
2024-05-02 08:36:02 -0400

    fixed appsync macro

--- transformations/aws/macros/appsync/appsync_should_have_logging_turned_on.sql
+++ transformations/aws/macros/appsync/appsync_should_have_logging_turned_on.sql
@@ -60,9 +60,9 @@ select
     account_id, 
     arn as resource_id,
     CASE
-        WHEN (log_config:cloudWatchLogsRoleArn IS NULL OR log_config:cloudWatchLogsRoleArn::STRING = '')
+        WHEN (json_extract_scalar(log_config, '$.cloudWatchLogsRoleArn') IS NULL OR json_extract_scalar(log_config, '$.cloudWatchLogsRoleArn') = '')
             OR 
-            log_config:fieldLogLevel = 'NONE' THEN 'fail'
+            json_extract_scalar(log_config, '$.fieldLogLevel') = 'NONE' THEN 'fail'
         ELSE 'pass'
     END AS status
 FROM aws_appsync_graphql_apis
