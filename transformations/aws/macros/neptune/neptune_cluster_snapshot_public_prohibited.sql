dfc5dff5534472fa3de8361e71dbe3bb292e6cec
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-05-13 10:33:14 +0300

    fix: Updated macro neptune_cluster_snapshot_public_prohibited

--- transformations/aws/macros/neptune/neptune_cluster_snapshot_public_prohibited.sql
+++ transformations/aws/macros/neptune/neptune_cluster_snapshot_public_prohibited.sql
@@ -29,11 +29,12 @@ select
     account_id,
     arn as resource_id,
     case when
-    attributes[0]:AttributeName = 'restore' and ARRAY_CONTAINS('all'::variant, attributes[0]:AttributeValues) then 'fail'
+    a.value:AttributeName = 'restore' and ARRAY_CONTAINS('all'::variant, attributes[0]:AttributeValues) then 'fail'
     else 'pass'
     end as status
 from 
-    aws_neptune_cluster_snapshots
+    aws_neptune_cluster_snapshots,
+    LATERAL FLATTEN(attributes) a
 {% endmacro %}
 
 {% macro bigquery__neptune_cluster_snapshot_public_prohibited(framework, check_id) %}
