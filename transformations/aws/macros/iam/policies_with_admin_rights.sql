9c259ecbd09e7c026f8ad02d25f51551f8d9c1ae
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-07-30 17:34:27 +0300

    fix: Updated AWS Compliance Packs to use aws_iam_policy_default_version (#986)
    * fix: Updated AWS Compliance Packs to use aws_iam_policy_default_version
    
    * Updated macros
    
    * Updated macro no_star

--- transformations/aws/macros/iam/policies_with_admin_rights.sql
+++ transformations/aws/macros/iam/policies_with_admin_rights.sql
@@ -3,65 +3,23 @@
 {% endmacro %}
 
 {% macro snowflake__policies_with_admin_rights(framework, check_id) %}
-with bad_statements as (
-SELECT
-    p.id
-FROM
-    aws_iam_policies p
-    INNER JOIN aws_iam_policy_versions pv ON pv._cq_parent_id = p._cq_id
-    , lateral flatten(input => pv.document_json:Statement) as s
-where pv.is_default_version = 'true' AND s.value:Effect = 'Allow'
-    and s.value:Effect = 'Allow'
-            and (s.value:Action = '*' or s.value:Action = '*:*')
-            and s.value:Resource = '*'
-)
-select
-    '{{framework}}' As framework,
-    '{{check_id}}' As check_id,
-    'IAM policies should not allow full * administrative privileges' as title,
-    account_id,
-    arn as resource_id,
-    CASE
-        WHEN b.id is not null THEN 'fail'
-        ELSE 'pass'
-    END as status
-from
-    aws_iam_policies as p
-LEFT JOIN bad_statements as b
-    ON p.id = b.id
-WHERE p.arn REGEXP '.*\d{12}.*'
-{% endmacro %}
-
-{% macro postgres__policies_with_admin_rights(framework, check_id) %}
-with iam_policies as (
+with pvs as (
     select
-        p.id as id,
+        p.id,
         pv.document_json as document
     from aws_iam_policies p
-    inner join aws_iam_policy_versions pv ON pv._cq_parent_id = p._cq_id
-    where pv.is_default_version = true and p.arn not like 'arn:aws:iam::aws:policy%'
-),
-policy_statements as (
+    inner join aws_iam_policy_default_versions pv on pv._cq_parent_id = p._cq_id
+), violations as (
     select
         id,
-        JSONB_ARRAY_ELEMENTS(
-            case JSONB_TYPEOF(document -> 'Statement')
-                when 'string' then JSONB_BUILD_ARRAY(document ->> 'Statement')
-                when 'array' then document -> 'Statement' end
-        ) as statement
-    from
-        iam_policies
-),
-allow_all_statements as (
-    select
-        id,
-        COUNT(statement) as statements_count
-    from policy_statements
-    where (statement ->> 'Action' = '*'
-        or statement ->> 'Action' like '%"*"%')
-        and statement ->> 'Effect' = 'Allow'
-        and (statement ->> 'Resource' = '*'
-            or statement ->> 'Resource' like '%"*"%')
+        COUNT(*) as violations
+    from pvs,
+        LATERAL FLATTEN(document:Statement) as statement,
+        LATERAL FLATTEN(statement.value:Resource) as resource,
+        LATERAL FLATTEN(statement.value:Action) as action
+    where statement.value:Effect = 'Allow'
+          and resource.value = '*'
+          and action.value = '*'
     group by id
 )
 
@@ -69,45 +27,98 @@ select distinct
     '{{framework}}' as framework,
     '{{check_id}}' as check_id,
     'IAM policies should not allow full ''*'' administrative privileges' as title,
-    aws_iam_policies.account_id,
-    aws_iam_policies.arn AS resource_id,
-    CASE WHEN statements_count > 0 THEN 'fail' ELSE 'pass' END AS status
+    account_id,
+    arn AS resource_id,
+    case when
+        violations.id is not null AND violations.violations > 0
+    then 'fail' else 'pass' end as status
 from aws_iam_policies
-left join
-    allow_all_statements on aws_iam_policies.id = allow_all_statements.id
+left join violations on violations.id = aws_iam_policies.id
+{% endmacro %}
+
+{% macro postgres__policies_with_admin_rights(framework, check_id) %}
+with pvs as (
+    select
+        p.id,
+        pv.document_json as document
+    from aws_iam_policies p
+    inner join aws_iam_policy_default_versions pv on pv._cq_parent_id = p._cq_id
+	where pv.is_default_version = true and p.arn not like 'arn:aws:iam::aws:policy%'
+), violations as (
+    select
+        id,
+        COUNT(*) as violations
+    from pvs,
+        JSONB_ARRAY_ELEMENTS(
+            case JSONB_TYPEOF(document -> 'Statement')
+                when 'string' then JSONB_BUILD_ARRAY(document ->> 'Statement')
+                when 'array' then document -> 'Statement'
+            end
+        ) as statement,
+        JSONB_ARRAY_ELEMENTS_TEXT(
+            case JSONB_TYPEOF(statement -> 'Resource')
+                when 'string' then JSONB_BUILD_ARRAY(statement ->> 'Resource')
+                when 'array' then statement -> 'Resource' end
+        ) as resource,
+        JSONB_ARRAY_ELEMENTS_TEXT( case JSONB_TYPEOF(statement -> 'Action')
+                when 'string' then JSONB_BUILD_ARRAY(statement ->> 'Action')
+                when 'array' then statement -> 'Action' end
+        ) as action
+    where 
+	(action = '*'
+        or action like '%"*"%')
+        and statement ->> 'Effect' = 'Allow'
+        and (resource = '*'
+            or resource like '%"*"%')
+    group by id
+)
+select distinct
+    '{{framework}}' as framework,
+    '{{check_id}}' as check_id,
+	'IAM policies should not allow full ''*'' administrative privileges' as title,
+    account_id,
+    arn AS resource_id,
+    case when
+        violations.id is not null AND violations.violations > 0
+    then 'fail' else 'pass' end as status
+from aws_iam_policies
+left join violations on violations.id = aws_iam_policies.id
 {% endmacro %}
 
 {% macro default__policies_with_admin_rights(framework, check_id) %}{% endmacro %}
 
 {% macro bigquery__policies_with_admin_rights(framework, check_id) %}
-with bad_statements as (
-SELECT
-    p.id
-FROM
-    {{ full_table_name("aws_iam_policies") }} p
-    INNER JOIN {{ full_table_name("aws_iam_policy_versions") }} pv 
-    ON pv._cq_parent_id = p._cq_id, 
-    UNNEST(JSON_QUERY_ARRAY(pv.document_json.Statement)) AS s
-where pv.is_default_version = true AND JSON_VALUE(s.Effect) = 'Allow'
-    and JSON_VALUE(s.Effect) = 'Allow'
-            and (JSON_VALUE(s.Action) = '*' or JSON_VALUE(s.Action) = '*:*')
-            and JSON_VALUE(s.Resource) = '*'
+with pvs as (
+    select
+        p.id,
+        pv.document_json as document
+    from {{ full_table_name("aws_iam_policies") }} p
+    inner join {{ full_table_name("aws_iam_policy_default_versions") }} pv on pv._cq_parent_id = p._cq_id
+), violations as (
+    select
+        id,
+        COUNT(*) as violations
+    from pvs,
+    UNNEST(JSON_QUERY_ARRAY(document.Statement)) AS statement,
+    UNNEST(JSON_QUERY_ARRAY(statement.Resource)) AS resource,
+    UNNEST(JSON_QUERY_ARRAY(statement.Action)) AS action
+    where JSON_VALUE(statement.Effect) = 'Allow'
+          and JSON_VALUE(resource) = '*'
+          and JSON_VALUE(action) = '*'
+    group by id
 )
-select
-    '{{framework}}' As framework,
-    '{{check_id}}' As check_id,
-    'IAM policies should not allow full * administrative privileges' as title,
+
+select distinct
+    '{{framework}}' as framework,
+    '{{check_id}}' as check_id,
+    CONCAT('IAM policies should not allow full ', '''*''', ' administrative privileges') AS title,
     account_id,
-    arn as resource_id,
-    CASE
-        WHEN b.id is not null THEN 'fail'
-        ELSE 'pass'
-    END as status
-from
-    {{ full_table_name("aws_iam_policies") }} as p
-LEFT JOIN bad_statements as b
-    ON p.id = b.id
-WHERE REGEXP_CONTAINS(p.arn, r'.*\d{12}.*')
+    arn AS resource_id,
+    case when
+        violations.id is not null AND violations.violations > 0
+    then 'fail' else 'pass' end as status
+from {{ full_table_name("aws_iam_policies") }}
+left join violations on violations.id = aws_iam_policies.id
 {% endmacro %}
 
 {% macro athena__policies_with_admin_rights(framework, check_id) %}
@@ -124,7 +135,7 @@ WITH iam_policies AS (
       json_extract(pv.document_json, '$.Statement')
   END AS statement
     FROM aws_iam_policies p
-    JOIN aws_iam_policy_versions pv ON pv._cq_parent_id = p._cq_id
+    JOIN aws_iam_policy_default_versions pv ON pv._cq_parent_id = p._cq_id
     WHERE pv.is_default_version = TRUE and p.arn not like 'arn:aws:iam::aws:policy%'
 ),
 policy_statements AS (
--- transformations/aws/macros/iam/wildcard_access_policies.sql
+++ transformations/aws/macros/iam/wildcard_access_policies.sql
@@ -5,47 +5,50 @@
 {% macro default__wildcard_access_policies(framework, check_id) %}{% endmacro %}
 
 {% macro postgres__wildcard_access_policies(framework, check_id) %}
-
-with policy_statements as (
+with pvs as (
     select
         p.id,
-        JSONB_ARRAY_ELEMENTS(
-            case JSONB_TYPEOF(v.document_json -> 'Statement')
-                when
-                    'string' then JSONB_BUILD_ARRAY(v.document_json ->> 'Statement')
-                when
-                    'array' then v.document_json -> 'Statement'
-            end
-        ) as statement
-    from
-        aws_iam_policies p
-        inner join aws_iam_policy_versions pv on p.account_id = pv.account_id AND p.arn = pv.policy_arn
-    where p.arn not like 'arn:aws:iam::aws:policy%'
-),
-
-allow_all_statements as (
+        pv.document_json as document
+    from aws_iam_policies p
+    inner join aws_iam_policy_default_versions pv on pv._cq_parent_id = p._cq_id
+), violations as (
     select
         id,
-        COUNT(statement) as statements_count
-    from
-        policy_statements
-    where
-        statement ->> 'Effect' = 'Allow'
-        and (
-            statement ->> 'Action' like '%*%'
-            or statement ->> 'NotAction' like '%*%')
-    group by
-        id
+        COUNT(*) as violations
+    from pvs,
+        JSONB_ARRAY_ELEMENTS(
+            case JSONB_TYPEOF(document -> 'Statement')
+                when 'string' then JSONB_BUILD_ARRAY(document ->> 'Statement')
+                when 'array' then document -> 'Statement'
+            end
+        ) as statement,
+        JSONB_ARRAY_ELEMENTS_TEXT( case JSONB_TYPEOF(statement -> 'Action')
+                when 'string' then JSONB_BUILD_ARRAY(statement ->> 'Action')
+                when 'array' then statement -> 'Action' end
+        ) as action
+LEFT JOIN LATERAL (
+	SELECT JSONB_ARRAY_ELEMENTS_TEXT(
+		CASE JSONB_TYPEOF(statement -> 'NotAction')
+			WHEN 'string' THEN JSONB_BUILD_ARRAY(statement ->> 'NotAction')
+			WHEN 'array' THEN statement -> 'NotAction'
+		END
+	) AS not_action
+) AS not_action_query ON TRUE
+where statement ->> 'Effect' = 'Allow'
+          and action like '%:*'
+          or not_action like '%:*'
+    group by id
 )
 
 select distinct
     '{{framework}}' as framework,
     '{{check_id}}' as check_id,
     'IAM customer managed policies that you create should not allow wildcard actions for services' as title,
-    aws_iam_policies.account_id,
-    aws_iam_policies.arn AS resource_id,
-    CASE WHEN statements_count > 0 THEN 'fail' ELSE 'pass' END AS status
+    account_id,
+    arn AS resource_id,
+    case when
+        violations.id is not null AND violations.violations > 0
+    then 'fail' else 'pass' end as status
 from aws_iam_policies
-     left join
-     allow_all_statements on aws_iam_policies.id = allow_all_statements.id
+left join violations on violations.id = aws_iam_policies.id
 {% endmacro %}
