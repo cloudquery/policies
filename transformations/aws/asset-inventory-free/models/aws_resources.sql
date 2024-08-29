c2754f7a4e6dae95653870a333b245bee267959c
k-rheinheimer <114611405+k-rheinheimer@users.noreply.github.com>
2024-08-29 11:49:52 -0400

    fix: Removing line breaks (#1052)
    * removing line breaks
    
    * updating versions
    
    * adding skip dependent tables to test to shorten length
    
    * adding more whitespace trimming
    
    * more whitespace trimming

--- transformations/aws/asset-inventory-free/models/aws_resources.sql
+++ transformations/aws/asset-inventory-free/models/aws_resources.sql
@@ -2,10 +2,9 @@
     {{ aws_tables_dyn() }}
 {% endset %}
 
--- Generate dynamic SQL statements
-{% for row in run_query(aws_tables) -%}
-    {% if row.table_name is not none and row.table_name != '' -%}
+{%- for row in run_query(aws_tables) -%}
+    {%- if row.table_name is not none and row.table_name != '' -%}
         {{ aws_asset_resources(row.table_name, row.arn_exist, row.account_id_exist, row.request_account_id_exist, row.region_exist, row.tags_exist) }}
-        {% if not loop.last -%} UNION ALL {% endif -%}
-    {%- endif %}
-{%- endfor %}
+        {%- if not loop.last -%} UNION ALL {% endif -%}
+    {%- endif -%}
+{%- endfor -%}
