8a940a86ff5d10a77da0da0bbdce3719c502f5b2
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2024-10-11 16:27:55 +0300

    feat: Added macros for ClickHouse destination (#1090)
    # Creating a Docker container for ClickHouse DB
    `docker run -d --name clickhouse-server -p 9000:9000 -p 8123:8123 clickhouse/clickhouse-server`
    
    # Installing nano in the container to edit files
    ```
    docker exec -it clickhouse-server bash   
    apt update
    apt install nano -y
    ```
    
    # Modifying the configuration file
    `nano /etc/clickhouse-server/users.xml`
    
    ```
    <profiles>
        <default>
            <max_query_size>1000000</max_query_size>
            <max_ast_elements>150000</max_ast_elements>
        </default>
    </profiles>
    ```
    
    # Restarting the container to apply changes
    `docker restart clickhouse-server`

--- transformations/aws/asset-inventory-free/macros/aws_tables_dyn.sql
+++ transformations/aws/asset-inventory-free/macros/aws_tables_dyn.sql
@@ -21,7 +21,6 @@ GROUP BY
 HAVING
     MAX(CASE WHEN UPPER(c.column_name) = 'ARN' THEN 1 ELSE 0 END)::bool
     AND (MAX(CASE WHEN UPPER(c.column_name) = 'ACCOUNT_ID' THEN 1 ELSE 0 END)::bool OR MAX(CASE WHEN UPPER(c.column_name) = 'REQUEST_ACCOUNT_ID' THEN 1 ELSE 0 END)::bool)
-
 {% endmacro %}    
 
 {% macro snowflake__aws_tables_dyn() %}
@@ -67,4 +66,27 @@ HAVING
     CAST(MAX(CASE WHEN UPPER(c.column_name) = 'ARN' THEN 1 ELSE 0 END) AS BOOL)
     AND (CAST(MAX(CASE WHEN UPPER(c.column_name) = 'ACCOUNT_ID' THEN 1 ELSE 0 END) as BOOL) OR CAST(MAX(CASE WHEN UPPER(c.column_name) = 'REQUEST_ACCOUNT_ID' THEN 1 ELSE 0 END) AS BOOL))
 
-{% endmacro %}  
+{% endmacro %}
+
+{% macro clickhouse__aws_tables_dyn() %}
+SELECT 
+    c.table AS table_name,
+    MAX(CASE WHEN c.name = 'arn' THEN 1 ELSE 0 END) AS arn_exist,
+    MAX(CASE WHEN c.name = 'account_id' THEN 1 ELSE 0 END) AS account_id_exist,
+    MAX(CASE WHEN c.name = 'request_account_id' THEN 1 ELSE 0 END) AS request_account_id_exist,
+    MAX(CASE WHEN c.name = 'region' THEN 1 ELSE 0 END) AS region_exist,
+    MAX(CASE WHEN c.name = 'tags' THEN 1 ELSE 0 END) AS tags_exist
+FROM 
+    system.columns c
+JOIN 
+    system.tables t ON c.database = t.database AND c.table = t.name
+WHERE
+    t.name LIKE 'aws_%'
+    AND t.engine NOT IN ('View', 'MaterializedView', 'LiveView')
+GROUP BY 
+    c.table
+HAVING
+    MAX(CASE WHEN c.name = 'arn' THEN 1 ELSE 0 END) = 1
+    AND (MAX(CASE WHEN c.name = 'account_id' THEN 1 ELSE 0 END) = 1 
+         OR MAX(CASE WHEN c.name = 'request_account_id' THEN 1 ELSE 0 END) = 1)
+{% endmacro %}
