38297f84ab8136ea46793c567e01d3ea8b3f18f0
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2025-03-21 10:30:09 +0000

    fix: Modify ECS.5 to correctly surface container_definition
    Previous grouping / predicate now applied via a JOIN.

--- transformations/aws/macros/ecs/containers_limited_read_only_root_filesystems.sql
+++ transformations/aws/macros/ecs/containers_limited_read_only_root_filesystems.sql
@@ -5,27 +5,45 @@
 {% macro default__containers_limited_read_only_root_filesystems(framework, check_id) %}{% endmacro %}
 
 {% macro postgres__containers_limited_read_only_root_filesystems(framework, check_id) %}
-with flat_containers as (
-        SELECT
-            arn,
-            account_id,
-            CASE
-                WHEN (container_definition ->> 'readonlyRootFilesystem')::BOOLEAN = FALSE
-                OR container_definition ->> 'readonlyRootFilesystem' IS NULL THEN 1
-                ELSE 0
-            END AS status,
-            task_role_arn,
-            max(revision) AS latest_revision
-        FROM
-            aws_ecs_task_definitions,
-			JSONB_ARRAY_ELEMENTS(container_definitions) as container_definition
-        WHERE
-            status = 'ACTIVE'
-        GROUP BY
-            arn,
-            account_id,
-            task_role_arn
-    )
+with latest_revisions as (
+    SELECT
+        arn,
+        account_id,
+        task_role_arn,
+        max(revision) AS latest_revision
+    FROM
+        aws_ecs_task_definitions
+    WHERE
+        status = 'ACTIVE'
+    GROUP BY
+        arn,
+        account_id,
+        task_role_arn
+),
+flat_containers as (
+    SELECT
+        t.arn,
+        t.account_id,
+        CASE
+            WHEN (container_definition ->> 'readonlyRootFilesystem')::BOOLEAN = FALSE
+            OR container_definition ->> 'readonlyRootFilesystem' IS NULL THEN 1
+            ELSE 0
+        END AS status,
+        t.task_role_arn,
+        lr.latest_revision
+    FROM
+        latest_revisions lr
+    JOIN
+        aws_ecs_task_definitions t
+    ON
+        lr.arn = t.arn
+        AND lr.account_id = t.account_id
+        AND lr.task_role_arn = t.task_role_arn
+        AND lr.latest_revision = t.revision,
+        JSONB_ARRAY_ELEMENTS(t.container_definitions) as container_definition
+    WHERE
+        t.status = 'ACTIVE'
+)
 select
     '{{framework}}' As framework,
     '{{check_id}}' As check_id,
@@ -41,27 +59,44 @@ from
 {% endmacro %}
 
 {% macro snowflake__containers_limited_read_only_root_filesystems(framework, check_id) %}
-with flat_containers as (
+with latest_revisions as (
         SELECT
             arn,
             account_id,
-            CASE
-                WHEN container_definition.value:readonlyRootFilesystem::BOOLEAN = FALSE
-                OR container_definition.value:readonlyRootFilesystem IS NULL THEN 1
-                ELSE 0
-            END AS status,
             task_role_arn,
-            max(revision) AS latest_revision
+            max (revision) AS latest_revision
         FROM
-            aws_ecs_task_definitions,
-            LATERAL FLATTEN(input => container_definitions) AS container_definition
+            aws_ecs_task_definitions
         WHERE
             status = 'ACTIVE'
         GROUP BY
             arn,
             account_id,
             task_role_arn
-    )
+), flat_containers as (
+        SELECT
+             t.arn,
+             t.account_id,
+             CASE
+                 WHEN container_definition.value:readonlyRootFilesystem::BOOLEAN = FALSE
+                 OR container_definition.value:readonlyRootFilesystem IS NULL THEN 1
+                 ELSE 0
+             END AS status,
+             t.task_role_arn,
+             lr.latest_revision
+        FROM
+             latest_revisions lr
+        JOIN
+             aws_ecs_task_definitions t
+        ON
+             lr.arn = t.arn
+             AND lr.account_id = t.account_id
+             AND lr.task_role_arn = t.task_role_arn
+             AND lr.latest_revision = t.revision,
+             LATERAL FLATTEN(input => t.container_definitions) AS container_definition
+        WHERE
+            t.status = 'ACTIVE'
+)
 select
     '{{framework}}' As framework,
     '{{check_id}}' As check_id,
@@ -77,27 +112,45 @@ from
 {% endmacro %}
 
 {% macro bigquery__containers_limited_read_only_root_filesystems(framework, check_id) %}
-with flat_containers as (
-        SELECT
-            arn,
-            account_id,
-            CASE
-                WHEN CAST( JSON_VALUE(container_definition.readonlyRootFilesystem) AS BOOL) = FALSE
-                OR JSON_VALUE(container_definition.readonlyRootFilesystem) IS NULL THEN 1
-                ELSE 0
-            END AS status,
-            task_role_arn,
-            max(revision) AS latest_revision
-        FROM
-            {{ full_table_name("aws_ecs_task_definitions") }},
-            UNNEST(JSON_QUERY_ARRAY(container_definitions)) AS container_definition
-        WHERE
-            status = 'ACTIVE'
-        GROUP BY
-            arn,
-            account_id,
-            task_role_arn
-    )
+with latest_revisions as (
+    SELECT
+        arn,
+        account_id,
+        task_role_arn,
+        max(revision) AS latest_revision
+    FROM
+        {{ full_table_name("aws_ecs_task_definitions") }}
+    WHERE
+        status = 'ACTIVE'
+    GROUP BY
+        arn,
+        account_id,
+        task_role_arn
+),
+flat_containers as (
+    SELECT
+        t.arn,
+        t.account_id,
+        CASE
+            WHEN CAST(JSON_VALUE(container_definition.readonlyRootFilesystem) AS BOOL) = FALSE
+            OR JSON_VALUE(container_definition.readonlyRootFilesystem) IS NULL THEN 1
+            ELSE 0
+        END AS status,
+        t.task_role_arn,
+        lr.latest_revision
+    FROM
+        latest_revisions lr
+    JOIN
+        {{ full_table_name("aws_ecs_task_definitions") }} t
+    ON
+        lr.arn = t.arn
+        AND lr.account_id = t.account_id
+        AND lr.task_role_arn = t.task_role_arn
+        AND lr.latest_revision = t.revision,
+        UNNEST(JSON_QUERY_ARRAY(t.container_definitions)) AS container_definition
+    WHERE
+    t.status = 'ACTIVE'
+)
 select
     '{{framework}}' As framework,
     '{{check_id}}' As check_id,
@@ -107,34 +160,52 @@ select
     CASE
         WHEN max(status) OVER (PARTITION BY arn) = 1 THEN 'fail'
         ELSE 'pass'
-    END as status
+        END as status
 from
     flat_containers
 {% endmacro %}
 
 {% macro athena__containers_limited_read_only_root_filesystems(framework, check_id) %}
 select * from (
-with flat_containers as (
-        SELECT
-            arn,
-            account_id,
-            CASE
-                WHEN cast(json_extract_scalar(container_definition, '$.readonlyRootFilesystem') as BOOLEAN) = FALSE
+with latest_revisions as (
+    SELECT
+        arn,
+        account_id,
+        task_role_arn,
+        max(revision) AS latest_revision
+    FROM
+        aws_ecs_task_definitions
+    WHERE
+        status = 'ACTIVE'
+    GROUP BY
+        arn,
+        account_id,
+        task_role_arn
+),
+flat_containers as (
+    SELECT
+        t.arn,
+        t.account_id,
+        CASE
+            WHEN cast(json_extract_scalar(container_definition, '$.readonlyRootFilesystem') as BOOLEAN) = FALSE
                 OR json_extract_scalar(container_definition, '$.readonlyRootFilesystem') IS NULL THEN 1
-                ELSE 0
+            ELSE 0
             END AS status,
-            task_role_arn,
-            max(revision) AS latest_revision
-        FROM
-            aws_ecs_task_definitions,
-            unnest(cast(json_parse(container_definitions) as array(json))) as t(container_definition)
-        WHERE
-            status = 'ACTIVE'
-        GROUP BY
-            arn,
-            account_id,
-            task_role_arn
-    )
+        t.task_role_arn,
+        lr.latest_revision
+    FROM
+        latest_revisions lr
+            JOIN
+        aws_ecs_task_definitions t
+        ON
+            lr.arn = t.arn
+                AND lr.account_id = t.account_id
+                AND lr.task_role_arn = t.task_role_arn
+                AND lr.latest_revision = t.revision,
+        unnest(cast(json_parse(t.container_definitions) as array(json))) as t(container_definition)
+    WHERE
+        t.status = 'ACTIVE'
+)
 select
     '{{framework}}' As framework,
     '{{check_id}}' As check_id,
